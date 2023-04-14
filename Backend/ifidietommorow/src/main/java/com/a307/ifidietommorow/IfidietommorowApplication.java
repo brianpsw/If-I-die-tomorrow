966f0366e87331ef9eb208117f04e1d38b74f515
Bluuubery <zephyr72836@gmail.com>
2023-04-14 16:23:56 +0900

    😀[]feat: initialize pjt

---
+++ Backend/ifidietommorow/src/main/java/com/a307/ifidietommorow/IfidietommorowApplication.java
@@ -0,0 +1,13 @@
+package com.a307.ifidietommorow;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IfidietommorowApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(IfidietommorowApplication.class, args);
+	}
+
+}
---
+++ Backend/ifidietommorow/src/test/java/com/a307/ifidietommorow/IfidietommorowApplicationTests.java
@@ -0,0 +1,13 @@
+package com.a307.ifidietommorow;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class IfidietommorowApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
