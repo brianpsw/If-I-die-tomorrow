9a688a3d28aa8dee87e9d722eab0f0e1de355794
Bluuubery <zephyr72836@gmail.com>
2023-04-17 10:47:35 +0900

    😀[BE]feat: userentity 작성

--- Backend/ifidietommorow/src/main/java/com/a307/ifidietommorow/domain/entity/User.java
+++ Backend/ifidietommorow/src/main/java/com/a307/ifidietommorow/domain/entity/User.java
@@ -1,4 +1,55 @@
 package com.a307.ifidietommorow.domain.entity;
 
-public class User {
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.*;
+
+@Entity
+@Getter
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+@Table(name = "user")
+public class User extends BaseEntity{
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(nullable = false)
+    private Long userId;
+
+    @Column(nullable = false)
+    private String name;
+
+    @Column(nullable = false)
+    private String email;
+
+    @Column(nullable = false)
+    private Integer age;
+
+    @Column(nullable = false)
+    private String nickname;
+
+    @Column(nullable = false)
+    private Boolean sendAgree;
+
+    @Column // 비동의 시 null로?
+    private String personalPage;
+
+    @Column // 가입 이후 검사 전까지는 null
+    private Long personalityId;
+
+    @Column(nullable = false)
+    private Boolean newCheck;
+
+    @Column(nullable = false)
+    private Boolean deleted;
+
+//    토큰 관련 부분은 확실하지 않아서 작성하고 주석처리했습니다.
+//    @Column
+//    private String refreshToken;
+
+
 }
