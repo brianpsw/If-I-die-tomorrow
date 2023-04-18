27fed8d7b1044606878eb341aaf20622e8940e07
Bluuubery <zephyr72836@gmail.com>
2023-04-18 16:52:45 +0900

    😀[BE]fix: 이것저것 수정
    controller -> restcontroller
    request mapping 추가

--- Backend/ifIDieTomorrow/src/main/java/com/a307/ifIDieTomorrow/domain/controller/DiaryController.java
+++ Backend/ifIDieTomorrow/src/main/java/com/a307/ifIDieTomorrow/domain/controller/DiaryController.java
@@ -7,21 +7,23 @@ import com.a307.ifIDieTomorrow.global.exception.NotFoundException;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.multipart.MultipartFile;
 
 import java.io.IOException;
 
-@Controller
+@RestController
 //@Api(value = "다이어리 관련 컨트롤러")
-@RequestMapping
+@RequestMapping("/diary")
 @RequiredArgsConstructor
 public class DiaryController {
 
 	private final DiaryService diaryService;
 
+	@GetMapping("")
 	public ResponseEntity<DiaryCreateResDto> createDiary(
 			@RequestBody MultipartFile photo,
 			@RequestBody DiaryCreateReqDto req
