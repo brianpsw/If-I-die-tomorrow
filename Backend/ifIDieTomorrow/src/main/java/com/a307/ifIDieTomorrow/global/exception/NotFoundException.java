08009eb22d386019ef519fd47af5e18df82c5320
Bluuubery <zephyr72836@gmail.com>
2023-04-18 13:35:26 +0900

    😀[BE]feat: notfound 예외처리

--- Backend/ifIDieTomorrow/src/main/java/com/a307/ifIDieTomorrow/global/exception/NotFoundException.java
+++ Backend/ifIDieTomorrow/src/main/java/com/a307/ifIDieTomorrow/global/exception/NotFoundException.java
@@ -1,4 +1,11 @@
 package com.a307.ifIDieTomorrow.global.exception;
 
-public class NotFoundException {
+import lombok.NoArgsConstructor;
+
+@NoArgsConstructor
+public class NotFoundException extends Exception{
+
+	public NotFoundException(String message) {
+		super (message);
+	}
 }
