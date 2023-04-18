411d538ba3e0ef7ce094973fe6779e9f8bd38df2
Bluuubery <zephyr72836@gmail.com>
2023-04-18 10:01:21 +0900

    😀[BE]feat: init new backend pjt

---
+++ Backend/ifIDieTomorrow/src/main/java/com/a307/ifIDieTomorrow/IfIDieTomorrowApplication.java
@@ -0,0 +1,13 @@
+package com.a307.ifIDieTomorrow;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IfIDieTomorrowApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(IfIDieTomorrowApplication.class, args);
+	}
+
+}
---
+++ Backend/ifIDieTomorrow/src/test/java/com/a307/ifIDieTomorrow/IfIDieTomorrowApplicationTests.java
@@ -0,0 +1,13 @@
+package com.a307.ifIDieTomorrow;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class IfIDieTomorrowApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
