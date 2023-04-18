78c68048ac1f5cd9bf796c52bc2868fad17e8623
Bluuubery <zephyr72836@gmail.com>
2023-04-18 16:34:16 +0900

    😀[BE]feat: diary create api 생성

---
+++ Backend/ifIDieTomorrow/src/main/java/com/a307/ifIDieTomorrow/domain/controller/DiaryController.java
@@ -0,0 +1,34 @@
+package com.a307.ifIDieTomorrow.domain.controller;
+
+import com.a307.ifIDieTomorrow.domain.dto.diary.DiaryCreateReqDto;
+import com.a307.ifIDieTomorrow.domain.dto.diary.DiaryCreateResDto;
+import com.a307.ifIDieTomorrow.domain.service.DiaryService;
+import com.a307.ifIDieTomorrow.global.exception.NotFoundException;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.multipart.MultipartFile;
+
+import java.io.IOException;
+
+@Controller
+//@Api(value = "다이어리 관련 컨트롤러")
+@RequestMapping
+@RequiredArgsConstructor
+public class DiaryController {
+
+	private final DiaryService diaryService;
+
+	public ResponseEntity<DiaryCreateResDto> createDiary(
+			@RequestBody MultipartFile photo,
+			@RequestBody DiaryCreateReqDto req
+			) throws IOException, NotFoundException
+	{
+		return ResponseEntity.status(HttpStatus.CREATED).body(diaryService.createDiary(req, photo));
+	}
+
+
+}
