aaed7ddeffe4909c0916753f8715d9e14958b716
박상우 <brianpsw@naver.com>
2023-04-19 13:48:00 +0900

    Update Jenkinsfile.groovy

--- Jenkinsfile.groovy
+++ Jenkinsfile.groovy
@@ -8,7 +8,11 @@ pipeline {
             steps {
                 echo "Running ${env.gitlabSourceBranch} on ${env.gitlabTargetBranch}"
                 echo "Clone ${env.gitlabActionType} ,  "
-                sh "ls -al"
+                sh '''
+                cd Frontend/frontend
+                ls -al
+
+                '''
                 git branch: "${env.gitlabSourceBranch}", credentialsId: 'test2', url: 'https://lab.ssafy.com/s08-final/S08P31A307.git'
             }
         }
