b446f0a93b8b60507676935960f0c493b3b7e6a3
Bluuubery <zephyr72836@gmail.com>
2023-04-19 16:55:58 +0900

    😀[BE]feat: 한 유저의 다이어리 목록 조회 컨트롤러 생성

--- Backend/ifIDieTomorrow/src/main/java/com/a307/ifIDieTomorrow/domain/controller/DiaryController.java
+++ Backend/ifIDieTomorrow/src/main/java/com/a307/ifIDieTomorrow/domain/controller/DiaryController.java
@@ -2,6 +2,7 @@ package com.a307.ifIDieTomorrow.domain.controller;
 
 import com.a307.ifIDieTomorrow.domain.dto.diary.CreateDiaryReqDto;
 import com.a307.ifIDieTomorrow.domain.dto.diary.CreateDiaryResDto;
+import com.a307.ifIDieTomorrow.domain.dto.diary.GetDiaryResDto;
 import com.a307.ifIDieTomorrow.domain.service.DiaryService;
 import com.a307.ifIDieTomorrow.global.exception.NotFoundException;
 import lombok.RequiredArgsConstructor;
@@ -11,6 +12,7 @@ import org.springframework.web.bind.annotation.*;
 import org.springframework.web.multipart.MultipartFile;
 
 import java.io.IOException;
+import java.util.List;
 
 @RestController
 //@Api(value = "다이어리 관련 컨트롤러")
@@ -22,11 +24,17 @@ public class DiaryController {
 
 	@PostMapping ("")
 	public ResponseEntity<CreateDiaryResDto> createDiary(
-			@RequestBody MultipartFile photo,
-			@RequestBody CreateDiaryReqDto req
+			@RequestPart(required = false) MultipartFile photo,
+			@RequestPart CreateDiaryReqDto req
 			) throws IOException, NotFoundException
 	{
 		return ResponseEntity.status(HttpStatus.CREATED).body(diaryService.createDiary(req, photo));
+	}
+
+//	이후 jwt 적용 시 수정
+	@GetMapping("/{userId}")
+	public ResponseEntity<List<GetDiaryResDto>> getDiaryByUserId(@PathVariable Long userId) throws NotFoundException {
+		return ResponseEntity.status(HttpStatus.OK).body(diaryService.getDiaryByUserId(userId));
 	}
 
 
