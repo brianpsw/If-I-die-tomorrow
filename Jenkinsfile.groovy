1d9f234470dbcf0175a84fb1c0f77842477beb3d
박상우 <brianpsw@naver.com>
2023-04-19 14:02:54 +0900

    Update Jenkinsfile.groovy

--- Jenkinsfile.groovy
+++ Jenkinsfile.groovy
@@ -7,7 +7,7 @@ pipeline {
 
             steps {
                 echo "Running ${env.gitlabSourceBranch} on ${env.gitlabTargetBranch}"
-                echo "Clone ${env.gitlabActionType} ,  "
+                echo "Clone ${env.gitlabActionType} ,   "
                 sh '''
                 cd Frontend/frontend
                 ls -al
