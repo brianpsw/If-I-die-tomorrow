c26667419e9e8eaba542f6a86a2ee8afaae5a493
LEEYEONSOO <noprob1325@naver.com>
2023-04-20 13:55:12 +0900

    👻[BE]fix: getWill 수정

--- Backend/ifIDieTomorrow/src/main/java/com/a307/ifIDieTomorrow/domain/repository/WillRepository.java
+++ Backend/ifIDieTomorrow/src/main/java/com/a307/ifIDieTomorrow/domain/repository/WillRepository.java
@@ -7,10 +7,12 @@ import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
+import java.util.Optional;
+
 @Repository
 public interface WillRepository extends JpaRepository<Will, Long> {
 	@Query("SELECT new com.a307.ifIDieTomorrow.domain.dto.will.GetWillByUserResDto(willId, content, videoUrl, voiceUrl, signUrl) " +
 			"FROM Will " +
 			"WHERE userId = :userId " )
-	GetWillByUserResDto findByUserId (@Param("userId") Long userId);
+	Optional<GetWillByUserResDto> findByUserId (@Param("userId") Long userId);
 }
