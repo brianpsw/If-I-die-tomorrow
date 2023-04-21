e9ba0805a1776efb6746d85aa3654bbf1d483283
LEEYEONSOO <noprob1325@naver.com>
2023-04-21 16:52:59 +0900

    👻[BE]feat: 포토 클라우드의 category 삭제 구현

--- Backend/ifIDieTomorrow/src/main/java/com/a307/ifIDieTomorrow/domain/repository/PhotoRepository.java
+++ Backend/ifIDieTomorrow/src/main/java/com/a307/ifIDieTomorrow/domain/repository/PhotoRepository.java
@@ -4,6 +4,11 @@ import com.a307.ifIDieTomorrow.domain.entity.Photo;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
+
 @Repository
 public interface PhotoRepository extends JpaRepository<Photo, Long> {
+	List<Long> findAllPhotoIdByCategory_CategoryId (Long categoryId);
+	
+	void deleteAllInBatchByPhotoIdIn (List<Long> allByCategory_categoryId);
 }
--- Backend/ifIDieTomorrow/src/main/java/com/a307/ifIDieTomorrow/domain/service/PhotoServiceImpl.java
+++ Backend/ifIDieTomorrow/src/main/java/com/a307/ifIDieTomorrow/domain/service/PhotoServiceImpl.java
@@ -5,6 +5,7 @@ import com.a307.ifIDieTomorrow.domain.dto.category.CreateCategoryResDto;
 import com.a307.ifIDieTomorrow.domain.dto.category.UpdateCategoryDto;
 import com.a307.ifIDieTomorrow.domain.entity.Category;
 import com.a307.ifIDieTomorrow.domain.repository.CategoryRepository;
+import com.a307.ifIDieTomorrow.domain.repository.PhotoRepository;
 import com.a307.ifIDieTomorrow.global.exception.NotFoundException;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
@@ -14,6 +15,8 @@ import org.springframework.stereotype.Service;
 public class PhotoServiceImpl implements PhotoService {
 	
 	private final CategoryRepository categoryRepository;
+	
+	private final PhotoRepository photoRepository;
 	
 	///////////////////////
 	// APIs For CATEGORY //
@@ -37,6 +40,20 @@ public class PhotoServiceImpl implements PhotoService {
 		category.updateCategory(data.getName());
 		
 		return CreateCategoryResDto.toDto(categoryRepository.save(category));
+	}
+	
+	@Override
+	public Long deleteCategory (Long categoryId) throws NotFoundException {
+		Category category = categoryRepository.findByCategoryId(categoryId)
+				.orElseThrow(() -> new NotFoundException("존재하지 않는 카테고리 ID 입니다."));
+		
+		// 카테고리에 엮인 사진 모두 삭제
+		photoRepository.deleteAllInBatchByPhotoIdIn(photoRepository.findAllPhotoIdByCategory_CategoryId(categoryId));
+		
+		// 카테고리 삭제
+		categoryRepository.delete(category);
+		
+		return categoryId;
 	}
 	
 	//////////////////////////
