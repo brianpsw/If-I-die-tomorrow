3938718d464bd94f48631a3706794c148b3fadf0
isks2 <isks2@naver.com>
2023-04-21 17:45:15 +0900

    👀[FE] feat: navbar생성 및 각 페이지에 적용

--- Frontend/frontend/src/pages/Diary/Diary.tsx
+++ Frontend/frontend/src/pages/Diary/Diary.tsx
@@ -1,7 +1,10 @@
+import NavBar from '../../components/common/NavBar';
+
 function Diary() {
   return (
     <>
       <h2>diary</h2>
+      <NavBar />
     </>
   );
 }
--- Frontend/frontend/src/pages/Feed/Feed.tsx
+++ Frontend/frontend/src/pages/Feed/Feed.tsx
@@ -1,7 +1,10 @@
+import NavBar from '../../components/common/NavBar';
+
 function Feed() {
   return (
     <>
       <h2>Feed입니다.</h2>
+      <NavBar />
     </>
   );
 }
