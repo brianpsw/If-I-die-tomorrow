8f274a368c5f14bbb6956db818b9d132b0a57ea3
Bluuubery <zephyr72836@gmail.com>
2023-04-22 16:11:40 +0900

    😀[BE]feat: 댓글 생성 서비스 구현

--- Backend/ifIDieTomorrow/src/main/java/com/a307/ifIDieTomorrow/domain/service/CommunityServiceImpl.java
+++ Backend/ifIDieTomorrow/src/main/java/com/a307/ifIDieTomorrow/domain/service/CommunityServiceImpl.java
@@ -1,10 +1,14 @@
 package com.a307.ifIDieTomorrow.domain.service;
 
+import com.a307.ifIDieTomorrow.domain.dto.comment.CreateCommentReqDto;
+import com.a307.ifIDieTomorrow.domain.dto.comment.CreateCommentResDto;
 import com.a307.ifIDieTomorrow.domain.dto.community.GetBucketWithCommentDto;
 import com.a307.ifIDieTomorrow.domain.dto.community.GetDiaryWithCommentDto;
+import com.a307.ifIDieTomorrow.domain.entity.Comment;
 import com.a307.ifIDieTomorrow.domain.repository.BucketRepository;
 import com.a307.ifIDieTomorrow.domain.repository.CommentRepository;
 import com.a307.ifIDieTomorrow.domain.repository.DiaryRepository;
+import com.a307.ifIDieTomorrow.domain.repository.UserRepository;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
@@ -20,6 +24,7 @@ public class CommunityServiceImpl implements CommunityService{
 	private final BucketRepository bucketRepository;
 	private final DiaryRepository diaryRepository;
 	private final CommentRepository commentRepository;
+	private final UserRepository userRepository;
 	
 	@Override
 	public List<GetBucketWithCommentDto> getBucketWithComments(){
@@ -40,8 +45,24 @@ public class CommunityServiceImpl implements CommunityService{
 				.stream()
 				.map(dto -> GetDiaryWithCommentDto.builder()
 						.diary(dto)
-						.comments(commentRepository.findCommentsByTypeId(dto.getDiaryId(), false))
+						.comments(commentRepository.findCommentsByTypeId(dto.getDiaryId(), true))
 						.build())
 				.collect(Collectors.toList());
+	}
+
+	@Override
+	public CreateCommentResDto createComment(CreateCommentReqDto req) {
+
+		Comment comment = commentRepository.save(req.toEntity());
+
+		return CreateCommentResDto.builder()
+				.commentId(comment.getCommentId())
+				.content(comment.getContent())
+				.nickname(userRepository.findUserNickNameByUserId(comment.getUserId()))
+				.type(comment.getType())
+				.typeId(comment.getTypeId())
+				.createdAt(comment.getCreatedAt())
+				.updatedAt(comment.getUpdatedAt())
+				.build();
 	}
 }
