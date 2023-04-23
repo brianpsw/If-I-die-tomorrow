de31de1ad61c77cc976817f40db4268e78b390d2
haneejo <haandeng@naver.com>
2023-04-23 18:07:07 +0900

    🍔[FE] feat: 설문조사 페이지 구현 및 css

---
+++ Frontend/frontend/src/pages/Diary/Question.tsx
@@ -0,0 +1,107 @@
+import React from 'react';
+import styled from 'styled-components';
+import tw from 'twin.macro';
+
+interface QuestionProps {
+  id: number;
+  title: string;
+  onChange: (id: number, value: number) => void;
+}
+
+const RadioButton = styled.input`
+  // 라디오 버튼 숨기기
+  display: none;
+
+  // 라디오 버튼 가상 요소 스타일
+  & + span {
+    display: inline-block;
+    width: 24px;
+    height: 24px;
+    border: 2px solid black;
+    border-radius: 50%;
+    position: relative;
+    cursor: pointer;
+  }
+
+  // 선택된 라디오 버튼 색상 변경
+  &:checked + span {
+    background-color: #ffa9a9;
+    border: 2px solid black;
+  }
+
+  // 각 크기에 대한 스타일
+  &.large + span {
+    width: 24px;
+    height: 24px;
+  }
+
+  &.medium + span {
+    width: 18px;
+    height: 18px;
+  }
+
+  &.small + span {
+    width: 12px;
+    height: 12px;
+  }
+`;
+
+const StyledQuestion = styled.div`
+  ${tw`mb-6 p-6 bg-opacity-70`}
+  background-color: #F6F6F6;
+  border-radius: 10px;
+  text-align: center;
+`;
+
+const QuestionTitle = styled.h2`
+  ${tw`mb-4 font-bold`}
+  font-size: 14px;
+`;
+
+const RadioGroup = styled.div`
+  ${tw`flex items-center justify-between`}
+`;
+
+const RadioLabel = styled.label`
+  ${tw`flex items-center space-x-2`}
+`;
+
+const Question: React.FC<QuestionProps> = ({ id, title, onChange }) => {
+  const handleRadioChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    onChange(id, Number(e.target.value));
+  };
+
+  const getRadioButtonSize = (index: number) => {
+    if (index === 0 || index === 4) {
+      return 'large';
+    } else if (index === 1 || index === 3) {
+      return 'medium';
+    } else {
+      return 'small';
+    }
+  };
+
+  return (
+    <StyledQuestion>
+      <QuestionTitle>{title}</QuestionTitle>
+      <RadioGroup>
+        <span>동의</span>
+        {Array.from({ length: 5 }, (_, index) => (
+          <RadioLabel key={index}>
+            <RadioButton
+              className={getRadioButtonSize(index)}
+              type="radio"
+              name={`question-${id}`}
+              value={index + 1}
+              onChange={handleRadioChange}
+            />
+            <span></span>
+          </RadioLabel>
+        ))}
+        <span>비동의</span>
+      </RadioGroup>
+    </StyledQuestion>
+  );
+};
+
+export default Question;
--- Frontend/frontend/src/pages/Diary/Survey.tsx
+++ Frontend/frontend/src/pages/Diary/Survey.tsx
@@ -1,9 +1,68 @@
-function Survey() {
-  return (
-    <>
-      <h2>유저성향조사 페이지</h2>
-    </>
-  );
+import React, { useState } from 'react';
+import styled from 'styled-components';
+import tw from 'twin.macro';
+import Question from './Question';
+import Button from '../../components/common/Button';
+
+const questions = [
+  '주기적으로 새로운 친구들을 만든다.',
+  '압박감이 심한 환경에서도 평정심을 유지하는 편이다.',
+  '일정이나 목록으로 계획을 세우는 일을 좋아한다.',
+  '작은 실수로도 자신의 능력이나 지식을 의심하곤 한다.',
+  '예술 작품의 다양한 해석에 대해 토록하는 일에는 크게 관심이 없다.',
+];
+
+interface PersonalityTestProps {
+  onSubmit?: (answers: { [key: string]: number }) => void;
 }
+
+const SurveyForm = styled.form`
+  ${tw`flex flex-col items-center mx-auto`}
+  max-width: calc(100% - 48px);
+`;
+
+const QuestionWrapper = styled.div`
+  ${tw`w-full`}
+`;
+
+const SurveyText = styled.p`
+  text-align: center;
+`;
+
+const Survey: React.FC<PersonalityTestProps> = ({ onSubmit }) => {
+  const [answers, setAnswers] = useState<{ [key: string]: number }>({});
+
+  const handleChange = (id: number, value: number) => {
+    setAnswers((prev) => ({ ...prev, [`q${id}`]: value }));
+  };
+
+  const handleSubmit = (e: React.FormEvent<HTMLFormElement>) => {
+    e.preventDefault();
+    onSubmit && onSubmit(answers);
+  };
+
+  return (
+    <SurveyForm onSubmit={handleSubmit}>
+      <SurveyText>
+        성향 설문조사를 통해 나에게 맞는
+        <br />
+        추천 버킷리스트를 받아보세요
+      </SurveyText>
+      <QuestionWrapper>
+        {questions.map((title, index) => (
+          <Question
+            key={index}
+            id={index + 1}
+            title={title}
+            onChange={handleChange}
+          />
+        ))}
+      </QuestionWrapper>
+      <Button color="#FFA9A9" size="md">
+        선택완료
+      </Button>
+    </SurveyForm>
+  );
+};
 
 export default Survey;
