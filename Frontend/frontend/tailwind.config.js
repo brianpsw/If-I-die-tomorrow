befb58b231a409a3210715abfed2e0900ef64d81
dayong2eyo <dayong2eyo@gmail.com>
2023-04-24 15:52:36 +0900

    💩[FE]fix: 테일윈드 오작동 고침

--- Frontend/frontend/tailwind.config.js
+++ Frontend/frontend/tailwind.config.js
@@ -1,5 +1,6 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
+  purge: ['./src/**/*.{js,jsx,ts,tsx}', './public/index.html'],
   content: [
     './src/pages//*.{html,js,jsx,ts,tsx}',
     './src/components//*.{html,js,jsx,ts,tsx}',
