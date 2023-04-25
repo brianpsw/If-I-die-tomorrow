acfa1265f9e68c48da2b4e50c8bbeb7e205925ea
Bluuubery <zephyr72836@gmail.com>
2023-04-25 13:20:11 +0900

    😀[BE]feat: 유저 작성 다이어리 레포지토리 메서드 테스트 작성

--- Backend/ifIDieTomorrow/src/test/java/com/a307/ifIDieTomorrow/domain/repository/DiaryRepositoryTest.java
+++ Backend/ifIDieTomorrow/src/test/java/com/a307/ifIDieTomorrow/domain/repository/DiaryRepositoryTest.java
@@ -3,17 +3,18 @@ package com.a307.ifIDieTomorrow.domain.repository;
 import com.a307.ifIDieTomorrow.domain.dto.diary.GetDiaryByUserResDto;
 import com.a307.ifIDieTomorrow.domain.entity.Comment;
 import com.a307.ifIDieTomorrow.domain.entity.Diary;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
 
 import java.util.List;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
 
-@SpringBootTest
+@DataJpaTest
 class DiaryRepositoryTest {
 
 	@Autowired
@@ -21,55 +22,98 @@ class DiaryRepositoryTest {
 	@Autowired
 	private CommentRepository testCommentRepository;
 
+	@BeforeEach
+	void setUp() {
+
+//		1번 유저 다이어리 (1, 2)
+		Diary diary1 = testDiaryRepository.save(
+				Diary.builder()
+						.userId(1L)
+						.diaryId(1L)
+						.title("1-1 title")
+						.content("1-1 content")
+						.imageUrl("")
+						.report(0)
+						.secret(true)
+						.build()
+		);
+		Diary diary2 = testDiaryRepository.save(
+				Diary.builder()
+						.userId(1L)
+						.diaryId(2L)
+						.title("1-2 title")
+						.content("1-2 content")
+						.imageUrl("")
+						.report(0)
+						.secret(false)
+						.build()
+		);
+
+//		2번 유저 다이어리 (3, 4)
+		Diary diary3 = testDiaryRepository.save(
+				Diary.builder()
+						.userId(2L)
+						.diaryId(3L)
+						.title("2-1 title")
+						.content("2-1 content")
+						.imageUrl("")
+						.report(0)
+						.secret(true)
+						.build()
+		);
+		Diary diary4 = testDiaryRepository.save(
+				Diary.builder()
+						.userId(2L)
+						.diaryId(4L)
+						.title("2-2 title")
+						.content("2-2 content")
+						.imageUrl("")
+						.report(0)
+						.secret(false)
+						.build()
+		);
+
+//		댓글 (1번 유저 1번 게시글에 1개, 1번유저 2번 게시글에 1개)
+		Comment comment1 = testCommentRepository.save(
+				Comment.builder()
+						.userId(3L)
+						.content("comment1")
+						.typeId(1L)
+						.type(true)
+						.build()
+		);
+
+		Comment comment2 = testCommentRepository.save(
+				Comment.builder()
+						.userId(3L)
+						.content("comment2")
+						.typeId(2L)
+						.type(true)
+						.build()
+		);
+
+
+	}
+
 	@Test
-	@DisplayName("해당 유저가 작성한 다이어리랑 댓글 개수 가져오기")
+	@DisplayName("특정 유저가 작성한 다이어리랑 댓글 개수 가져오기")
 	void findAllByUserIdWithCommentCount() {
 		// Given
 		Long userId = 1L;
 
-		Diary diary = Diary.builder()
-				.userId(userId)
-				.title("Test title")
-				.content("Test content")
-				.imageUrl("")
-				.build();
-
-		diary = testDiaryRepository.save(diary);
-
-//		해당 다이어리에 작성된 댓글
-		Comment comment1 = Comment.builder()
-				.userId(1L)
-				.content("comment1")
-				.typeId(diary.getDiaryId())
-				.type(true)
-				.build();
-		testCommentRepository.save(comment1);
-
-//		해당 다이어리에 작성되지 않은 댓글
-		Comment comment2 = Comment.builder()
-				.userId(1L)
-				.content("comment2")
-				.typeId(2L)
-				.type(true)
-				.build();
-		testCommentRepository.save(comment2);
-
 		// When
 		List<GetDiaryByUserResDto> result = testDiaryRepository.findAllByUserIdWithCommentCount(userId);
-		System.out.println(result);
 
 		// Then
-		assertThat(result).isNotNull();
-		assertThat(result).hasSize(1);
+//		다이어리 2개
+		assertThat(result).hasSize(2);
 
+//		1번 다이어리 게시글
 		GetDiaryByUserResDto dto = result.get(0);
-		assertThat(dto.getDiaryId()).isEqualTo(diary.getDiaryId());
-		assertThat(dto.getTitle()).isEqualTo(diary.getTitle());
-		assertThat(dto.getContent()).isEqualTo(diary.getContent());
-		assertThat(dto.getImageUrl()).isEqualTo(diary.getImageUrl());
-		assertThat(dto.getSecret()).isEqualTo(diary.getSecret());
-		assertThat(dto.getCreatedAt()).isEqualTo(diary.getCreatedAt());
-		assertThat(dto.getUpdatedAt()).isEqualTo(diary.getUpdatedAt());
+		assertThat(dto.getTitle()).isEqualTo("1-1 title");
+		assertThat(dto.getContent()).isEqualTo("1-1 content");
+		assertThat(dto.getImageUrl()).isEqualTo("");
+		assertThat(dto.getSecret()).isEqualTo(true);
 
 //		댓글 1개
 		assertThat(dto.getCommentCount()).isEqualTo(1L);
