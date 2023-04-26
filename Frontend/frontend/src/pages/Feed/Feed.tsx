dd7ade54240bde66344309202222a52543c904e5
haneejo <haandeng@naver.com>
2023-04-26 14:29:11 +0900

    🍔[FE]feat: 다이어리 피드 무한스크롤 구현

--- Frontend/frontend/src/pages/Feed/Feed.tsx
+++ Frontend/frontend/src/pages/Feed/Feed.tsx
@@ -3,13 +3,23 @@ import { Icon } from '@iconify/react';
 import { Link } from 'react-router-dom';
 import styled from 'styled-components';
 import tw from 'twin.macro';
-import backgroundImg from '../../assets/images/mypage_bg.jpg';
+import backgroundImg from '../../assets/images/feed.png';
 import Button from '../../components/common/Button';
 import DiaryFeed from '../../components/feed/DiaryFeed';
 import BucketFeed from '../../components/feed/BucketFeed';
 
+const Background = styled.div`
+  background-image: url(${backgroundImg});
+  background-size: cover;
+  background-position: center;
+  background-repeat: no-repeat;
+  min-height: 100vh;
+  width: 100%;
+  position: relative;
+`;
+
 const Container = styled.div`
-  ${tw`flex flex-col mx-auto pt-12`}
+  ${tw`flex flex-col mx-auto pt-12  pb-24`}
   max-width: calc(100% - 48px);
   text-align: center;
   justify-content: center;
@@ -18,34 +28,39 @@ const Container = styled.div`
 const FeedTab = styled.div`
   ${tw`flex`}
   justify-content: space-evenly;
+  color: white;
 `;
 
 const Tab = styled.span<{ isSelected: boolean }>`
-  ${tw`cursor-pointer mb-6`}
-  ${({ isSelected }) => (isSelected ? tw`font-bold border-b-2` : null)}
+  ${tw`cursor-pointer mb-2 inline-block pb-1`}
+  ${({ isSelected }) => (isSelected ? tw`font-bold border-b-4` : null)}
+  ${({ isSelected }) => (isSelected ? { borderColor: '#FFA9A9' } : null)}
+  width: 110px;
 `;
 
 function Feed() {
   const [feedType, setFeedType] = useState<'diary' | 'bucketList'>('diary');
 
   return (
-    <Container>
-      <FeedTab>
-        <Tab
-          isSelected={feedType === 'bucketList'}
-          onClick={() => setFeedType('bucketList')}
-        >
-          버킷리스트
-        </Tab>
-        <Tab
-          isSelected={feedType === 'diary'}
-          onClick={() => setFeedType('diary')}
-        >
-          다이어리
-        </Tab>
-      </FeedTab>
-      {feedType === 'diary' ? <DiaryFeed /> : <BucketFeed />}
-    </Container>
+    <Background>
+      <Container>
+        <FeedTab>
+          <Tab
+            isSelected={feedType === 'bucketList'}
+            onClick={() => setFeedType('bucketList')}
+          >
+            버킷리스트
+          </Tab>
+          <Tab
+            isSelected={feedType === 'diary'}
+            onClick={() => setFeedType('diary')}
+          >
+            다이어리
+          </Tab>
+        </FeedTab>
+        {feedType === 'diary' ? <DiaryFeed /> : <BucketFeed />}
+      </Container>
+    </Background>
   );
 }
 
