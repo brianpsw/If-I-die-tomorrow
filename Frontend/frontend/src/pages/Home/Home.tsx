b12a27c7ed943cd4610242d2741bf189636c540e
isks2 <isks2@naver.com>
2023-04-26 11:07:55 +0900

    👀[FE] feat: Home에 띄울 custom  model (semi room) 띄움

--- Frontend/frontend/src/pages/Home/Home.tsx
+++ Frontend/frontend/src/pages/Home/Home.tsx
@@ -1,9 +1,12 @@
 import React from 'react';
 
+import HomeSemiRoom from '../../components/home/HomeSemiRoom';
+
 function Home() {
   return (
     <div>
       <h2>Home입니다.</h2>
+      <HomeSemiRoom />
     </div>
   );
 }
