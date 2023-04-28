811ff4263ba6f9b87b6e234be2f12c4bb3ad1676
isks2 <isks2@naver.com>
2023-04-28 17:33:34 +0900

    👀[FE] feat: 사진 클라우드 카테고리 조회 api 연결

--- Frontend/frontend/src/components/PhotoCloud/PhotoCloudCategory.tsx
+++ Frontend/frontend/src/components/PhotoCloud/PhotoCloudCategory.tsx
@@ -1,40 +1,101 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
+import axios from 'axios';
 
-function PhotoCloudCategory() {
-  const dummyData = [
-    {
-      categoryId: 1,
-      name: '첫번째',
-    },
-    {
-      categoryId: 2,
-      name: '두번째',
-    },
-    {
-      categoryId: 3,
-      name: '세번째',
-    },
-    {
-      categoryId: 4,
-      name: '네번째',
-    },
-    {
-      categoryId: 5,
-      name: '다섯번째',
-    },
-    {
-      categoryId: 6,
-      name: '여섯번째',
-    },
-    {
-      categoryId: 7,
-      name: '일곱번째',
-    },
-  ];
+import requests from '../../api/config';
+
+interface CategoryInfo {
+  categoryId: number;
+  name: string;
+}
+
+interface PhotoCloudCategoryProps {
+  setSelectedCategory: React.Dispatch<React.SetStateAction<string>>;
+}
+
+function PhotoCloudCategory(props: PhotoCloudCategoryProps) {
+  const [categoryData, setCategoryData] = useState<CategoryInfo[] | null>(null);
+  // const dummyData = [
+  //   {
+  //     categoryId: 4,
+  //     name: '첫번째',
+  //     color: 'red',
+  //   },
+  //   {
+  //     categoryId: 5,
+  //     name: '두번째',
+  //     color: 'purple',
+  //   },
+  //   {
+  //     categoryId: 6,
+  //     name: '세번째',
+  //     color: 'pink',
+  //   },
+  //   {
+  //     categoryId: 7,
+  //     name: '네번째',
+  //     color: 'black',
+  //   },
+  //   {
+  //     categoryId: 8,
+  //     name: '다섯번째',
+  //     color: 'skyblue',
+  //   },
+  //   {
+  //     categoryId: 9,
+  //     name: '여섯번째',
+  //     color: 'orange',
+  //   },
+  //   {
+  //     categoryId: 10,
+  //     name: '일곱번째',
+  //     color: 'blue',
+  //   },
+  // ];
+
+  const fetchData = async () => {
+    try {
+      const response = await axios.get(`${requests.base_url}/photo/category`, {
+        withCredentials: true,
+      });
+      if (response.status === 200) {
+        const { data } = response;
+        setCategoryData(() => data);
+      }
+    } catch (err) {
+      console.error(err);
+    }
+  };
+
+  useEffect(() => {
+    fetchData();
+  }, []);
+
+  const handleCategory = (id: number) => {
+    props.setSelectedCategory(id.toString());
+  };
 
   return (
-    <div>
-      <p>포토클라우드 카테고리</p>
+    <div className="flex">
+      {categoryData ? (
+        categoryData.map((category: CategoryInfo) => {
+          return (
+            <div
+              style={{
+                border: '1px solid #111',
+                width: '30px',
+                height: '30px',
+                backgroundColor: 'white',
+              }}
+              key={category.categoryId}
+              onClick={() => handleCategory(category.categoryId)}
+            >
+              {category.categoryId}
+            </div>
+          );
+        })
+      ) : (
+        <p>loading...</p>
+      )}
     </div>
   );
 }
