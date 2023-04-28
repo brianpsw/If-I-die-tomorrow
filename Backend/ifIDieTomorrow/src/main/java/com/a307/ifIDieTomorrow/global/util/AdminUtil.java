ce5d7fb7541a8968bb23b1a095ff28f1bf9206ff
Bluuubery <zephyr72836@gmail.com>
2023-04-28 17:01:42 +0900

    😀[BE]feat: 리포트 횟수 변수 외부로 분리

---
+++ Backend/ifIDieTomorrow/src/main/java/com/a307/ifIDieTomorrow/global/util/AdminUtil.java
@@ -0,0 +1,13 @@
+package com.a307.ifIDieTomorrow.global.util;
+
+import lombok.Getter;
+import org.springframework.stereotype.Component;
+
+@Component
+@Getter
+public class AdminUtil {
+
+	public final Integer MAX_REPORT = 5;
+
+
+}
