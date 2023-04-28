da65694a33f9c29d821f2065b0f31c318c7fbc76
isks2 <isks2@naver.com>
2023-04-28 16:51:28 +0900

    👀[FE] feat: 포토 클라우드에 카테고리 디테일 컴포넌트 추가 및 카테고리 별 사진 불러오는 api 연결

---
+++ Frontend/frontend/src/components/PhotoCloud/PhotoCloudCategory.tsx
@@ -0,0 +1,42 @@
+import React from 'react';
+
+function PhotoCloudCategory() {
+  const dummyData = [
+    {
+      categoryId: 1,
+      name: '첫번째',
+    },
+    {
+      categoryId: 2,
+      name: '두번째',
+    },
+    {
+      categoryId: 3,
+      name: '세번째',
+    },
+    {
+      categoryId: 4,
+      name: '네번째',
+    },
+    {
+      categoryId: 5,
+      name: '다섯번째',
+    },
+    {
+      categoryId: 6,
+      name: '여섯번째',
+    },
+    {
+      categoryId: 7,
+      name: '일곱번째',
+    },
+  ];
+
+  return (
+    <div>
+      <p>포토클라우드 카테고리</p>
+    </div>
+  );
+}
+
+export default PhotoCloudCategory;
--- Frontend/frontend/src/pages/PhotoCloud/PhotoCloud.tsx
+++ Frontend/frontend/src/pages/PhotoCloud/PhotoCloud.tsx
@@ -1,8 +1,63 @@
+import React, { useState } from 'react';
+import { useNavigate } from 'react-router';
+import { Icon } from '@iconify/react';
+
+import PhotoCloudCategory from '../../components/PhotoCloud/PhotoCloudCategory';
+import PhotoCloudDetail from '../../components/PhotoCloud/PhotoCloudDetail';
+import EditOrDeleteModal from '../../components/common/EditOrDeleteModal';
+import { Background } from '../../pages/PhotoCloud/PhotoCloudEmotion';
+
 function PhotoCloud() {
+  const [openEditOrDeleteModal, setOpenEditOrDeleteModal] = useState(false);
+  const [openCaptionEditModal, setOpenCaptionEditModal] = useState(false);
+  const [deleteModalOpen, setDeleteModalOpen] = useState(false);
+  const [selectedPhotoId, setSelectedPhotoId] = useState('');
+  const [selectedCategory, setSelectedCategory] = useState('6');
+
+  const handleEditOrDeleteModalOpen = () => {
+    setOpenEditOrDeleteModal(true);
+  };
+  // 수정, 삭제 모달 close
+  const onEditOrDeleteModalClose = () => {
+    setOpenEditOrDeleteModal(false);
+  };
+
+  // 버킷 수정 모달 open
+  const handleCaptionEditModalOpen = () => {
+    setOpenCaptionEditModal(true);
+  };
+  // 버킷 수정 모달 close
+  const onCaptionEditModalClose = () => {
+    setOpenCaptionEditModal(false);
+  };
+
+  //삭제 모달 open
+  const handleDeleteModalOpen = () => {
+    setDeleteModalOpen(true);
+  };
+  //삭제 모달 close
+  const onDeleteModalClose = () => {
+    setDeleteModalOpen(false);
+  };
+
   return (
-    <>
-      <h2>포토클라우드</h2>
-    </>
+    <Background>
+      {openEditOrDeleteModal ? (
+        <EditOrDeleteModal
+          onClose={onEditOrDeleteModalClose}
+          handleBucketEditModalOpen={handleCaptionEditModalOpen}
+          handleDeleteModalOpen={handleDeleteModalOpen}
+        />
+      ) : null}
+      <PhotoCloudCategory
+      // setSelectedCategory={setSelectedCategory}
+      ></PhotoCloudCategory>
+      <PhotoCloudDetail
+        setOpenEditOrDeleteModal={setOpenEditOrDeleteModal}
+        setSelectedPhotoId={setSelectedPhotoId}
+        selectedCategory={selectedCategory}
+      ></PhotoCloudDetail>
+    </Background>
   );
 }
 
