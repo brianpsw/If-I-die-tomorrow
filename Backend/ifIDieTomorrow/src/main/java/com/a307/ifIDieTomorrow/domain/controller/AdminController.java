f43733a72a14e72c743193cca402d5ab1d561e7b
Bluuubery <zephyr72836@gmail.com>
2023-04-28 20:44:44 +0900

    😀[BE]feat: 신고 5회 이상 게시글 조회 컨트롤러 api매핑

---
+++ Backend/ifIDieTomorrow/src/main/java/com/a307/ifIDieTomorrow/domain/controller/AdminController.java
@@ -0,0 +1,32 @@
+package com.a307.ifIDieTomorrow.domain.controller;
+
+import com.a307.ifIDieTomorrow.domain.dto.admin.GetOverLimitResDto;
+import com.a307.ifIDieTomorrow.domain.service.AdminService;
+import com.a307.ifIDieTomorrow.global.exception.UnAuthorizedException;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.tags.Tag;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@Tag(name = "관리자", description = "APIs for Admin")
+@Slf4j
+@RestController
+@RequiredArgsConstructor
+@RequestMapping("/admin")
+public class AdminController {
+
+	private final AdminService adminService;
+
+	@GetMapping(value = "/report")
+	@Operation(summary = "신고 5회 이상 게시글 조회", description = "신고 횟수가 5회 이상 누적된 다이어리/버킷을 조회해 신고 많은 순으로 정렬해서 반환")
+	public ResponseEntity<List<GetOverLimitResDto>> getOverLimit() throws UnAuthorizedException {
+		return ResponseEntity.status(HttpStatus.OK).body(adminService.getBucketOrDiaryOverReportLimit());
+	}
+}
