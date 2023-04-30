4764e8f46fe289189758c776b4c0ba6c70936333
이연수 <noprob1325@naver.com>
2023-04-30 22:21:32 +0900

    👻[BE]feat: 유언장 영상 삭제 구현

--- Backend/ifIDieTomorrow/src/main/java/com/a307/ifIDieTomorrow/domain/controller/WillController.java
+++ Backend/ifIDieTomorrow/src/main/java/com/a307/ifIDieTomorrow/domain/controller/WillController.java
@@ -9,6 +9,7 @@ import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.tags.Tag;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.multipart.MultipartFile;
@@ -29,7 +30,7 @@ public class WillController {
 		return ResponseEntity.status(HttpStatus.OK).body(willService.getWillByUserId());
 	}
 	
-	@PatchMapping("/sign")
+	@PatchMapping(value = "/sign", consumes = { MediaType.MULTIPART_FORM_DATA_VALUE })
 	@Operation(summary = "서명 저장", description = "유저의 서명을 저장합니다.")
 	public ResponseEntity<Long> createSign(
 			@RequestPart MultipartFile photo) throws NoPhotoException, IOException, IllegalArgumentException {
@@ -43,11 +44,17 @@ public class WillController {
 		return ResponseEntity.status(HttpStatus.OK).body(willService.updateContent(content));
 	}
 	
-	@PatchMapping("/video")
+	@PatchMapping(value = "/video", consumes = { MediaType.MULTIPART_FORM_DATA_VALUE })
 	@Operation(summary = "유언장 영상 수정", description = "유저의 영상을 수정합니다.")
 	public ResponseEntity<Long> updateVideo(
 			@RequestPart MultipartFile video) throws IOException, IllegalArgumentException, NoPhotoException {
 		return ResponseEntity.status(HttpStatus.OK).body(willService.updateVideo(video));
+	}
+	
+	@DeleteMapping("/video")
+	@Operation(summary = "유언장 영상 삭제", description = "유저의 영상을 삭제합니다.")
+	public ResponseEntity<Long> deleteVideo() {
+		return ResponseEntity.status(HttpStatus.NO_CONTENT).body(willService.deleteVideo());
 	}
 	
 }
