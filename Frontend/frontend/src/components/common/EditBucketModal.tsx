df5a03a8401797bc3e2b5820a1fa7410fa6c02e5
haneejo <haandeng@naver.com>
2023-05-02 14:06:45 +0900

    🍔[FE]feat:버킷리스트 상세페이지

---
+++ Frontend/frontend/src/components/common/EditBucketModal.tsx
@@ -0,0 +1,140 @@
+import React, { useState } from 'react';
+import styled from 'styled-components';
+import tw from 'twin.macro';
+import axios from 'axios';
+import requests from '../../api/config';
+import { defaultApi } from '../../api/axios';
+
+const ModalOverlay = styled.div`
+  ${tw`flex items-center justify-center z-50 bg-neutral-400/80 h-full w-full fixed`}
+`;
+
+const ModalWrapper = styled.div`
+  ${tw`bg-white flex flex-col items-center absolute border-solid rounded-xl h-auto w-[380px] shadow mt-[50%] font-sans`}
+  bottom: 0;
+`;
+
+interface EditBucketModalProps {
+  bucketId: number;
+  title: string;
+  content: string;
+  secret: boolean;
+  onClose?: () => void;
+  onUpdate?: (updatedBucket: any) => void;
+}
+
+function EditBucketModal({
+  bucketId,
+  title,
+  content,
+  secret,
+  onClose,
+  onUpdate,
+}: EditBucketModalProps) {
+  const [newTitle, setNewTitle] = useState(title);
+  const [newContent, setNewContent] = useState(content);
+  const [newSecret, setNewSecret] = useState(secret);
+  const [photo, setPhoto] = useState<File | null>(null);
+  const [updatePhoto, setUpdatePhoto] = useState<boolean>(false);
+
+  const handleSubmit = async (e: React.FormEvent) => {
+    e.preventDefault();
+
+    try {
+      const formData = new FormData();
+      formData.append(
+        'data',
+        JSON.stringify({
+          bucketId,
+          title: newTitle,
+          content: newContent,
+          secret: newSecret,
+          updatePhoto, // updatePhoto 추가
+        }),
+      );
+
+      if (photo) {
+        formData.append('photo', photo); // 사진 파일이 있으면 formData에 추가
+      }
+
+      const response = await defaultApi.put(requests.POST_BUCKET(), formData, {
+        withCredentials: true,
+        headers: {
+          'Content-Type': 'multipart/form-data',
+        },
+      });
+
+      if (response.status === 200) {
+        if (onClose) {
+          onClose();
+        }
+        if (onUpdate) {
+          // Add this line
+          onUpdate(response.data); // Add this line
+        } // Add this line
+      }
+    } catch (error) {
+      console.error(error);
+    }
+  };
+  const handleFileChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    if (e.target.files && e.target.files.length > 0) {
+      setPhoto(e.target.files[0]);
+      setUpdatePhoto(true);
+    }
+  };
+
+  const handleInputChange = (
+    e: React.ChangeEvent<HTMLInputElement | HTMLTextAreaElement>,
+  ) => {
+    // Update this line
+    const { name, value } = e.target;
+    if (name === 'title') {
+      setNewTitle(value);
+    } else if (name === 'content') {
+      setNewContent(value);
+    }
+  };
+
+  const handleCheckboxChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    setNewSecret(e.target.checked);
+  };
+
+  return (
+    <ModalOverlay>
+      <ModalWrapper>
+        <h3>일기 수정</h3>
+        <form onSubmit={handleSubmit}>
+          <label htmlFor="title">제목</label>
+          <input
+            type="text"
+            id="title"
+            name="title"
+            value={newTitle}
+            onChange={handleInputChange}
+          />
+          <label htmlFor="photo">사진</label>
+          <input type="file" id="photo" onChange={handleFileChange} />
+          <label htmlFor="content">내용</label>
+          <textarea
+            id="content"
+            name="content"
+            value={newContent}
+            onChange={handleInputChange}
+          />
+          <label htmlFor="secret">비밀</label>
+          <input
+            type="checkbox"
+            id="secret"
+            checked={newSecret}
+            onChange={handleCheckboxChange}
+          />
+          <button type="submit">수정하기</button>
+        </form>
+        <button onClick={onClose}>닫기</button>
+      </ModalWrapper>
+    </ModalOverlay>
+  );
+}
+
+export default EditBucketModal;
--- Frontend/frontend/src/pages/Bucket/BucketDetail.tsx
+++ Frontend/frontend/src/pages/Bucket/BucketDetail.tsx
@@ -1,18 +1,21 @@
 import React, { useState, useEffect } from 'react';
 import axios from 'axios';
 import requests from '../../api/config';
-import { useParams } from 'react-router-dom';
+import { defaultApi } from '../../api/axios';
+import { useParams, useNavigate } from 'react-router-dom';
 import styled from 'styled-components';
 import tw from 'twin.macro';
 import backgroundImg from '../../assets/images/diary_bg.png';
 import TreeDot from '../../assets/icons/three_dot.svg';
+import EditOrDeleteModal from '../../components/common/EditOrDeleteModal';
+import EditBucketModal from '../../components/common/EditBucketModal';
 
 interface Comment {
   commentId: bigint;
   content: string;
   nickname: string;
-  created: string;
-  updated: string;
+  createdAt: string;
+  updatedAt: string;
 }
 
 interface Bucket {
@@ -21,8 +24,8 @@ interface Bucket {
   content: string;
   imageUrl: string;
   secret: boolean;
-  created: string;
-  updated: string;
+  createdAt: string;
+  updatedAt: string;
   nickname: string;
 }
 
@@ -75,12 +78,13 @@ const CommentWrap = styled.div`
   ${tw`mb-6 flex flex-col mx-auto`}
   max-width: calc(100% - 48px);
   border-radius: 10px;
-  color: white;
+  // color: white;
   // border: solid 1px white;
 `;
 
 const CommentBox = styled.div`
   ${tw`mb-2 p-6 flex`}
+  justify-content: space-between;
   color: black;
   background-color: rgba(246, 246, 246, 0.7);
   border-radius: 10px;
@@ -104,25 +108,72 @@ function BucketDetail() {
   const { bucketId } = useParams<{ bucketId: string }>();
   const [bucketDetail, setBucketDetail] = useState<Bucket | null>(null);
   const [comments, setComments] = useState<Comment[]>([]);
+  const [modalOpen, setModalOpen] = useState(false);
+  const [editModalOpen, setEditModalOpen] = useState(false);
+  const [updatePhoto, setUpdatePhoto] = useState<boolean>(false);
+  const navigate = useNavigate();
+
+  const handleModalOpen = () => {
+    setModalOpen(true);
+  };
+
+  const handleModalClose = () => {
+    setModalOpen(false);
+  };
+
+  const handleEditModalOpen = () => {
+    setEditModalOpen(true);
+  };
+
+  const handleEditModalClose = () => {
+    setEditModalOpen(false);
+  };
+
+  const handleDeleteModalOpen = () => {
+    if (window.confirm('정말로 삭제하시겠습니까?')) {
+      // 확인/취소 버튼이 있는 모달을 띄움
+      if (bucketId) {
+        deleteBucket(parseInt(bucketId));
+      } else {
+        console.error('Bucket ID is undefined');
+      }
+    }
+  };
+
+  const deleteBucket = async (bucketId: number) => {
+    try {
+      await defaultApi.delete(requests.DELETE_BUCKET(bucketId), {
+        withCredentials: true,
+      });
+      navigate('/feed?tab=bucket'); // 피드 페이지로 이동
+      setBucketDetail(null); // 상태를 업데이트하여 게시물이 화면에서 사라지도록 함
+      setComments([]); // 댓글도 함께 초기화
+      alert('다이어리가 삭제되었습니다.');
+    } catch (error) {
+      console.error(error);
+    }
+  };
+
+  const handleUpdate = (updatedBucket: Bucket) => {
+    setBucketDetail(updatedBucket);
+    setUpdatePhoto(true);
+  };
+
+  const fetchBucketDetail = async () => {
+    try {
+      const response = await defaultApi.get(requests.GET_BUCKET(bucketId), {
+        withCredentials: true,
+      });
+      if (response.status === 200) {
+        setBucketDetail(response.data.bucket);
+        setComments(response.data.comments);
+      }
+    } catch (error) {
+      console.error(error);
+    }
+  };
 
   useEffect(() => {
-    const fetchBucketDetail = async () => {
-      try {
-        const response = await axios.get(
-          `${requests.base_url}/bucket/${bucketId}`,
-          {
-            withCredentials: true,
-          },
-        );
-        if (response.status === 200) {
-          setBucketDetail(response.data.bucket);
-          setComments(response.data.comments);
-        }
-      } catch (error) {
-        console.error(error);
-      }
-    };
-
     fetchBucketDetail();
   }, [bucketId]);
 
@@ -133,33 +184,56 @@ function BucketDetail() {
   const bucket = bucketDetail;
 
   return (
-    <Background>
-      <Container>
-        <BucketWrap>
-          <BucketHeader>
-            <div>
-              <h2 className="text-h3">{bucket.title}</h2>
-              <Nickname>{bucket.nickname}</Nickname>
-            </div>
-            <DotIcon>
-              <img src={TreeDot} alt="" />
-            </DotIcon>
-          </BucketHeader>
-          <BucketImg>
-            {bucket.imageUrl && bucket.imageUrl !== '""' && (
-              <img src={bucket.imageUrl} alt="Bucket" />
-            )}
-          </BucketImg>
-          <BucketText>{bucket.content}</BucketText>
-        </BucketWrap>
-        <CommentWrap>
-          <CommentForm bucketId={bucket.bucketId} />
-          {comments.map((comment, index) => (
-            <Comment key={index} comment={comment} />
-          ))}
-        </CommentWrap>
-      </Container>
-    </Background>
+    <div>
+      {modalOpen && (
+        <EditOrDeleteModal
+          onClose={handleModalClose}
+          handleBucketEditModalOpen={handleEditModalOpen}
+          handleDeleteModalOpen={handleDeleteModalOpen}
+        />
+      )}
+      {editModalOpen && bucketDetail && (
+        <EditBucketModal
+          bucketId={bucketDetail.bucketId}
+          title={bucketDetail.title}
+          content={bucketDetail.content}
+          secret={bucketDetail.secret}
+          onClose={handleEditModalClose}
+          onUpdate={handleUpdate}
+        />
+      )}
+      <Background>
+        <Container>
+          <BucketWrap>
+            <BucketHeader>
+              <div>
+                <h2 className="text-h3">{bucket.title}</h2>
+                <Nickname>{bucket.nickname}</Nickname>
+              </div>
+              <DotIcon>
+                <img src={TreeDot} alt="" onClick={handleModalOpen} />
+              </DotIcon>
+            </BucketHeader>
+            <BucketImg>
+              {bucket.imageUrl && bucket.imageUrl !== '""' && (
+                <img src={bucket.imageUrl} alt="Bucket" />
+              )}
+            </BucketImg>
+            <BucketText>{bucket.content}</BucketText>
+          </BucketWrap>
+          <CommentWrap>
+            <CommentForm bucketId={bucket.bucketId} />
+            {comments.map((comment, index) => (
+              <Comment
+                key={index}
+                comment={comment}
+                onUpdate={() => fetchBucketDetail()}
+              />
+            ))}
+          </CommentWrap>
+        </Container>
+      </Background>
+    </div>
   );
 }
 
@@ -169,8 +243,8 @@ function CommentForm({ bucketId }: { bucketId: number }) {
   const handleSubmit = async (e: React.FormEvent) => {
     e.preventDefault();
     try {
-      const response = await axios.post(
-        `${requests.base_url}/board/comment`, // 수정된 API 엔드포인트
+      const response = await defaultApi.post(
+        requests.POST_COMMENT(), // 수정된 API 엔드포인트
         {
           content,
           type: true, // type을 true로 설정
@@ -198,13 +272,118 @@ function CommentForm({ bucketId }: { bucketId: number }) {
   );
 }
 
-function Comment({ comment }: { comment: Comment }) {
+function Comment({
+  comment,
+  onUpdate,
+}: {
+  comment: Comment;
+  onUpdate: () => void;
+}) {
+  const [modalOpen, setModalOpen] = useState(false);
+  const [editing, setEditing] = useState(false); // 추가: 댓글 수정 상태
+  // const [editedContent, setEditedContent] = useState(comment.content); // 추가: 수정된 댓글 내용
+  const [content, setContent] = useState(comment.content);
+
+  const handleModalOpen = () => {
+    setModalOpen(true);
+  };
+
+  const handleModalClose = () => {
+    setModalOpen(false);
+  };
+
+  const handleBucketEditModalOpen = () => {
+    setEditing(true); // 추가: 댓글 수정 모드 활성화
+    handleModalClose(); // 추가: 수정/삭제 모달 닫기
+  };
+
+  const handleChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    setContent(e.target.value);
+  };
+
+  const handleCancel = () => {
+    setEditing(false);
+    setContent(comment.content); // 원래 댓글 내용으로 되돌립니다.
+  };
+
+  const handleDeleteModalOpen = () => {
+    deleteComment(comment.commentId);
+    handleModalClose();
+  };
+
+  const deleteComment = async (commentId: bigint) => {
+    try {
+      const response = await defaultApi.delete(
+        requests.DELETE_COMMENT(commentId),
+        {
+          withCredentials: true,
+        },
+      );
+      if (response.status === 200) {
+        onUpdate(); // 댓글 목록을 업데이트하도록 함수 호출
+      }
+    } catch (error) {
+      console.error(error);
+    }
+  };
+
+  const updateComment = async (commentId: bigint, content: string) => {
+    try {
+      const response = await defaultApi.put(
+        requests.PUT_COMMENT(),
+        {
+          commentId,
+          content,
+        },
+        { withCredentials: true },
+      );
+      if (response.status === 200) {
+        setEditing(false);
+        setContent(content);
+        onUpdate(); // 댓글 목록을 업데이트하도록 함수 호출
+      }
+    } catch (error) {
+      console.error(error);
+    }
+  };
+
+  const handleEditSubmit = (e: React.FormEvent) => {
+    e.preventDefault(); // 폼의 기본 동작을 취소합니다.
+    updateComment(comment.commentId, content);
+  };
+
   return (
-    <CommentBox>
-      <b>{comment.nickname}</b>
-      <p>{comment.content}</p>
-      <p>{comment.created}</p>
-    </CommentBox>
+    <div>
+      {modalOpen && (
+        <EditOrDeleteModal
+          onClose={handleModalClose}
+          handleBucketEditModalOpen={handleBucketEditModalOpen}
+          handleDeleteModalOpen={handleDeleteModalOpen}
+        />
+      )}
+      <CommentBox>
+        <div>
+          <b>{comment.nickname}</b>
+          <p>{comment.createdAt}</p>
+        </div>
+        <div>
+          {editing ? (
+            <form onSubmit={handleEditSubmit}>
+              <input type="text" value={content} onChange={handleChange} />
+              <button type="submit">수정</button>
+              <button type="button" onClick={handleCancel}>
+                취소
+              </button>
+            </form>
+          ) : (
+            <p>{comment.content}</p>
+          )}
+        </div>
+        <DotIcon>
+          <img src={TreeDot} alt="" onClick={handleModalOpen} />
+        </DotIcon>
+      </CommentBox>
+    </div>
   );
 }
 
