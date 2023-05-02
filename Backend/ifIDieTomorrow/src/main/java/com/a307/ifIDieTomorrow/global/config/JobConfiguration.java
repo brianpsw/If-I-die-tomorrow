b615ad0a46fbb152e7e72b4f4841a32895517803
Bluuubery <zephyr72836@gmail.com>
2023-05-02 13:36:44 +0900

    😀[BE]feat: 스프링 배치 유저 일괄 처리하는 기능 구현

--- Backend/ifIDieTomorrow/src/main/java/com/a307/ifIDieTomorrow/global/config/JobConfiguration.java
+++ Backend/ifIDieTomorrow/src/main/java/com/a307/ifIDieTomorrow/global/config/JobConfiguration.java
@@ -15,6 +15,7 @@ import org.springframework.context.annotation.Configuration;
 
 import java.time.LocalDateTime;
 import java.util.List;
+import java.util.stream.Collectors;
 
 @Configuration
 @RequiredArgsConstructor
@@ -28,51 +29,79 @@ public class JobConfiguration {
 	@Bean
 	public Job job(){
 		return jobBuilderFactory.get("job")
-				.start(step1())
+				.start(processUser())
+				.next(finished())
 				.build();
 	}
 
 
 	@Bean
-	public Step step1(){
-		TaskletStep step1 = stepBuilderFactory.get("step1")
+	public Step processUser(){
+		TaskletStep processUser = stepBuilderFactory.get("processUser")
 				.tasklet((contribution, chunkContext) -> {
 
-					log.info(">>>>> step1");
-					List<User> users = userRepository.findAll();
+					log.info(">>>>> step processUser starts");
+
+					/**
+					 * 1. 사후 페이지가 없는 유저
+					 */
+					log.info(">>>>> 1.1: Fetch all users agreed to send service");
+					List<User> users = userRepository.findAllUsersWhereSendAgreeIsTrue()
+							.stream()
+							.filter(user -> user.getPersonalPage() == null)
+							.collect(Collectors.toList());
+
+					log.info("fetched {} users to process", users.size());
+
+					/**
+					 * 2. 6개월 이상 활동 안 한 유저
+					 */
+					log.info(">>>>> 1.2: six months ago");
+					LocalDateTime sixMonthsAgo = LocalDateTime.now().minusMonths(6);
+
+					users.parallelStream()
+							.filter(user -> user.getUpdatedAt().isBefore(sixMonthsAgo))
+							.forEach(this::sendPage);
+
+					/**
+					 * 3. 3개월 ~ 6개월 간 활동 안 한 유저
+					 */
+					log.info(">>>>> 1.3: three months ago");
+
+					LocalDateTime threeMonthsAgo = LocalDateTime.now().minusMonths(3);
+
+					users.parallelStream()
+							.filter(user -> user.getUpdatedAt().isAfter(sixMonthsAgo) && user.getUpdatedAt().isBefore(threeMonthsAgo))
+							.forEach(this::sendNotice);
 
 					return RepeatStatus.FINISHED;
 				})
 				.build();
-		step1.setAllowStartIfComplete(true);
-		return step1;
+		processUser.setAllowStartIfComplete(true);
+		return processUser;
 	}
 
-	private Boolean wasActiveInLast3Month(User user){
 
-		LocalDateTime threeMonthsAgo = LocalDateTime.now().minusMonths(3);
-
-		return user.getUpdatedAt().isAfter(threeMonthsAgo);
-
+	@Bean
+	public Step finished(){
+		return stepBuilderFactory.get("finished")
+				.tasklet((contribution, chunkContext) -> {
+					log.info("task finished at {}", LocalDateTime.now());
+					return RepeatStatus.FINISHED;
+				}).build();
 	}
 
-	private void sendMessage(User user){
+
+	private void sendNotice(User user){
 		// 알림을 보내는 뭔가
+		log.info("send Notice to {}", user.getNickname());
 	}
 
-	private Boolean wasActiveInLast6Month(User user){
-
-		LocalDateTime sixMonthsAgo = LocalDateTime.now().minusMonths(6);
-
-		return user.getUpdatedAt().isAfter(sixMonthsAgo);
-
-	}
 
 	private void sendPage(User user){
 		// 페이지를 발송하는 뭔가
+		log.info("send Page to receivers of {}", user.getNickname());
 	}
-
-
 
 
 }
