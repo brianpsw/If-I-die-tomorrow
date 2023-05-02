54370051439ea0ab39fed76f9995aa1c930a1b0b
Bluuubery <zephyr72836@gmail.com>
2023-05-02 13:25:01 +0900

    😀[BE]feat: 동의 전송 유저 조회 레포지토리 메서드 작성

--- Backend/ifIDieTomorrow/src/main/java/com/a307/ifIDieTomorrow/domain/repository/UserRepository.java
+++ Backend/ifIDieTomorrow/src/main/java/com/a307/ifIDieTomorrow/domain/repository/UserRepository.java
@@ -5,6 +5,7 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
 
+import java.util.List;
 import java.util.Optional;
 
 
@@ -20,5 +21,10 @@ public interface UserRepository extends JpaRepository<User, Long> {
 
 
     Optional<User> findByEmail(String email);
+
+	@Query("SELECT u " +
+			"FROM User u " +
+			"WHERE u.sendAgree = true")
+	List<User> findAllUsersWhereSendAgreeIsTrue();
 
 }
