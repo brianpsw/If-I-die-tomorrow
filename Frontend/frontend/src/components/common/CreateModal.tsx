f38567c15dc6260601bad02a3104c63a4bdc2531
dayong2eyo <dayong2eyo@gmail.com>
2023-05-02 17:11:02 +0900

    💩[FE]feat: 버킷 생성 api 연결

--- Frontend/frontend/src/components/common/CreateModal.tsx
+++ Frontend/frontend/src/components/common/CreateModal.tsx
@@ -2,6 +2,8 @@ import { useEffect, useRef, useState, ReactNode } from 'react';
 import useOutsideClick from '../../hooks/useOutsideClick';
 import styled from 'styled-components';
 import tw from 'twin.macro';
+import requests from '../../api/config';
+import { defaultApi } from '../../api/axios';
 import Button from './Button';
 
 const ModalOverlay = styled.div`
@@ -15,16 +17,55 @@ const ModalWrapper = styled.div`
 const ContentInputContainer = styled.textarea`
   ${tw`flex flex-wrap w-full pt-1 h-[33px] bg-white rounded border-black break-all my-4 px-[6px]`}
 `;
-
+interface Bucket {
+  bucketId: number;
+  title: string;
+  complete: string;
+  secret: boolean;
+}
 interface ModalProps {
+  setBuckets: React.Dispatch<React.SetStateAction<Bucket[]>>;
   onClose?: () => void;
 }
 
-function CreateModal({ onClose }: ModalProps) {
+function CreateModal({ onClose, setBuckets }: ModalProps) {
   const modalRef = useRef<HTMLDivElement>(null);
   const [contentValue, setContentValue] = useState('');
+  const [isValid, setIsValid] = useState(false);
+  const handleContentChange = (e: React.ChangeEvent<HTMLTextAreaElement>) => {
+    setContentValue(e.currentTarget.value);
+    setIsValid(e.currentTarget.value.length > 0);
+  };
   const handleSubmit = () => {
     //버킷리스트 추가 api 연결
+    const get_user_bucket = async () => {
+      try {
+        const response = await defaultApi.get(requests.GET_USER_BUCKET(), {
+          withCredentials: true,
+        });
+        setBuckets(response.data);
+        return console.log(response.data);
+      } catch (error) {
+        throw error;
+      }
+    };
+    const post_bucket = async () => {
+      try {
+        const response = await defaultApi.post(
+          requests.POST_BUCKET(),
+          contentValue,
+          {
+            withCredentials: true,
+          },
+        );
+
+        console.log(response.data);
+        get_user_bucket();
+      } catch (error) {
+        throw error;
+      }
+    };
+    post_bucket();
     onClose?.();
   };
   const handleClose = () => {
@@ -52,12 +93,24 @@ function CreateModal({ onClose }: ModalProps) {
     <ModalOverlay>
       <ModalWrapper ref={modalRef}>
         <ContentInputContainer
-          onChange={(e) => setContentValue(e.target.value)}
+          onChange={handleContentChange}
           value={contentValue}
           placeholder="버킷리스트 내용을 작성해주세요."
         />
+        {!isValid ? (
+          <span className="text-yellow-500 text-p2">
+            버킷리스트 내용을 입력해주세요.
+          </span>
+        ) : (
+          ''
+        )}
         <div className="flex w-full justify-center my-4">
-          <Button onClick={handleSubmit} color="#B3E9EB" size="sm">
+          <Button
+            onClick={handleSubmit}
+            color="#B3E9EB"
+            size="sm"
+            disabled={isValid ? false : true}
+          >
             작성 완료
           </Button>
         </div>
