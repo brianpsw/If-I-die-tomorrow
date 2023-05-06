bc8d6b644891a69447fd5f36e9ad7d5dfbbc0f51
SuGyoungIn <isks2838@gmail.com>
2023-05-06 14:24:06 +0900

    👀[FE]feat: 새로운 룸 오브젝트 추가, 드래그클릭이벤트 방지 구현

--- Frontend/frontend/src/components/home/HomeSemiRoom.tsx
+++ Frontend/frontend/src/components/home/HomeSemiRoom.tsx
@@ -1,4 +1,4 @@
-import React, { useState, Suspense } from 'react';
+import React, { useState, Suspense, useRef, useEffect } from 'react';
 
 import { useNavigate } from 'react-router';
 
@@ -6,50 +6,85 @@ import * as THREE from 'three';
 import { Canvas } from '@react-three/fiber';
 import { useGLTF, OrbitControls } from '@react-three/drei';
 
-function HomeSemiRoom() {
-  const navigate = useNavigate();
-  const room3d = useGLTF('models/low_poly_room.glb', true);
-  const heart = useGLTF('models/heart.glb', true);
-  const meshes = [...room3d.scene.children, ...heart.scene.children];
+interface PreventDragClick {
+  preventDragClick: boolean;
+}
 
-  const mouse = new THREE.Vector2();
-  const raycaster = new THREE.Raycaster();
+function Scene(props: PreventDragClick) {
+  const navigate = useNavigate();
+  const roomFrame = useGLTF('models/room_frame.glb', true);
+  const heart = useGLTF('models/heart.glb', true);
 
   const clickRoom = (e: any) => {
-    navigate('/photo-cloud/1');
+    if (!props.preventDragClick) navigate('/room');
     e?.stopPropagation();
   };
 
   const clickHeart = (e: any) => {
-    navigate('/photo-cloud/4');
+    if (!props.preventDragClick) navigate('/photo-cloud/4');
     e?.stopPropagation();
   };
 
   return (
+    <Suspense fallback={<div>loading...</div>}>
+      <directionalLight position={[30, 50, 100]} intensity={1.2} />
+
+      <primitive
+        object={heart.scene}
+        scale={[0.3, 0.3, 0.3]}
+        onClick={(e: any) => clickHeart(e)}
+      />
+      <primitive
+        object={roomFrame.scene}
+        scale={[50, 50, 50]}
+        position={[50, -200, -50]}
+        onClick={(e: any) => clickRoom(e)}
+      />
+
+      <OrbitControls />
+    </Suspense>
+  );
+}
+
+function HomeSemiRoom() {
+  const canvas = useRef<HTMLCanvasElement>(null);
+  const [preventDragClick, setPreventDragClick] = useState<boolean>(false);
+
+  useEffect(() => {
+    let nowCanvas = canvas.current;
+    let clickStartX: number;
+    let clickStartY: number;
+    let clickStartTime: number;
+
+    nowCanvas?.addEventListener('mousemove', (e) => {
+      clickStartX = e.clientX;
+      clickStartY = e.clientY;
+      clickStartTime = Date.now();
+    });
+
+    nowCanvas?.addEventListener('mouseup', (e) => {
+      const xGap = Math.abs(e.clientX - clickStartX);
+      const yGap = Math.abs(e.clientY - clickStartY);
+      const timeGap = Date.now() - clickStartTime;
+
+      if (xGap > 5 || yGap > 5 || timeGap > 300) {
+        setPreventDragClick(true);
+      } else {
+        setPreventDragClick(false);
+      }
+    });
+  }, []);
+
+  return (
     <Canvas
+      ref={canvas}
       style={{ width: '100vw', height: '92vh' }}
       orthographic
       camera={{
         position: [500, 300, 500],
       }}
     >
-      <Suspense fallback={<div>loading...</div>}>
-        <directionalLight position={[30, 50, 100]} intensity={1.2} />
-
-        <primitive
-          object={heart.scene}
-          scale={[0.3, 0.3, 0.3]}
-          onClick={(e: any) => clickHeart(e)}
-        />
-        <primitive
-          object={room3d.scene}
-          scale={[0.6, 0.8, 0.6]}
-          position={[0, -200, 0]}
-          onClick={(e: any) => clickRoom(e)}
-        />
-
-        <OrbitControls />
-      </Suspense>
+      <Scene preventDragClick={preventDragClick} />
     </Canvas>
   );
 }
