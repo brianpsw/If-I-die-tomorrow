dc110ad9e90f45e021819437e2f0a3d27501f0f8
SuGyoungIn <isks2838@gmail.com>
2023-05-15 01:05:19 +0900

    👀[FE]refactor: 안쓰는 recoil state 삭제

--- Frontend/frontend/src/states/CategoryState.tsx
+++ Frontend/frontend/src/states/CategoryState.tsx
@@ -5,29 +5,12 @@ const { persistAtom } = recoilPersist();
 interface CategoryData {
   categoryId: number;
   name: string;
-  objectId: number;
+  userId: number;
   imageUrl: string;
 }
 
 export const categoryState = atom<CategoryData[] | null>({
   key: 'categoryState',
   default: null,
-  effects_UNSTABLE: [persistAtom],
-});
-
-export const exchangeCategoryState = atom({
-  key: 'exchangeCategoryState',
-  default: {
-    1: '0 0',
-    2: '-60px 0',
-    3: '-120px 0',
-    4: '-180px 0',
-    5: '-240px 0',
-    6: '0 -60px',
-    7: '-60px -60px',
-    8: '-120px -60px',
-    9: '-180px -60px',
-    10: '-240px -60px',
-  },
   effects_UNSTABLE: [persistAtom],
 });
