e5039d396936a7880e28b4d4960f1c973478c8d4
Bluuubery <zephyr72836@gmail.com>
2023-05-15 14:00:56 +0900

    😀[BE]feat: 커뮤니티 페이징 기본값 들어가는지 검증

--- Backend/ifIDieTomorrow/src/test/java/com/a307/ifIDieTomorrow/domain/service/CommunityServiceImplTest.java
+++ Backend/ifIDieTomorrow/src/test/java/com/a307/ifIDieTomorrow/domain/service/CommunityServiceImplTest.java
@@ -13,11 +13,10 @@ import com.a307.ifIDieTomorrow.global.auth.UserPrincipal;
 import com.a307.ifIDieTomorrow.global.util.AdminUtil;
 import org.junit.jupiter.api.*;
 import org.junit.jupiter.api.extension.ExtendWith;
-import org.mockito.BDDMockito.*;
+import org.mockito.ArgumentCaptor;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.PageImpl;
 import org.springframework.data.domain.PageRequest;
@@ -31,11 +30,8 @@ import java.util.Collections;
 import java.util.List;
 
 import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
-import static org.junit.jupiter.api.Assertions.*;
 import static org.mockito.ArgumentMatchers.*;
-import static org.mockito.BDDMockito.given;
-import static org.mockito.BDDMockito.then;
-import static org.mockito.Mockito.never;
+import static org.mockito.BDDMockito.*;
 import static org.mockito.Mockito.times;
 
 @ExtendWith(MockitoExtension.class)
@@ -96,8 +92,10 @@ class CommunityServiceImplTest {
 				int pageNo = 0;
 				int pageSize = 10;
 
+//				페이지 요청
 				PageRequest pageable = PageRequest.of(pageNo, pageSize);
 
+//				버킷 1
 				GetBucketResDto bucketResDto1 = GetBucketResDto.builder()
 						.bucketId(1L)
 						.userId(1L)
@@ -110,6 +108,7 @@ class CommunityServiceImplTest {
 						.updatedAt(LocalDateTime.now())
 						.build();
 
+//				버킷 2
 				GetBucketResDto bucketResDto2 = GetBucketResDto.builder()
 						.bucketId(2L)
 						.userId(2L)
@@ -122,6 +121,7 @@ class CommunityServiceImplTest {
 						.updatedAt(LocalDateTime.now())
 						.build();
 
+//				버킷1의 댓글
 				GetCommentResDto commentResDto = GetCommentResDto.builder()
 						.commentId(1L)
 						.content("TestCommentContent")
@@ -133,16 +133,19 @@ class CommunityServiceImplTest {
 
 				List<GetCommentResDto> commentResDtoList = Collections.singletonList(commentResDto);
 
+//				버킷 1 + 댓글
 				GetBucketWithCommentDto bucketWithCommentDto1 = GetBucketWithCommentDto.builder()
 						.bucket(bucketResDto1)
 						.comments(commentResDtoList)
 						.build();
 
+//				버킷 2 + 댓글 없음
 				GetBucketWithCommentDto bucketWithCommentDto2 = GetBucketWithCommentDto.builder()
 						.bucket(bucketResDto2)
 						.comments(new ArrayList<>())
 						.build();
 
+//				예상 결괏값
 				List<GetBucketWithCommentDto> bucketWithCommentList = Arrays.asList(bucketWithCommentDto1, bucketWithCommentDto2);
 				Page<GetBucketResDto> pageResponse = new PageImpl<>(Arrays.asList(bucketResDto1, bucketResDto2));
 
@@ -156,16 +159,53 @@ class CommunityServiceImplTest {
 
 
 				// Then
+				/**
+				 * 게시글 조회
+				 * 게시글 당 댓글 조회
+				 */
 				then(bucketRepository).should().findAllBySecretIsFalseAndReportUnderLimit(pageable, adminUtil.MAX_REPORT);
-				then(commentRepository).should(times(2)).findCommentsByTypeId(anyLong(), anyBoolean());
+				then(commentRepository).should(times(bucketWithCommentList.size())).findCommentsByTypeId(anyLong(), anyBoolean());
 
+				/**
+				 * 결괏값 검증
+				 * 페이징 검증
+				 */
 				assertThat(result.getData()).isEqualTo(bucketWithCommentList);
 				assertThat(result.getHasNext()).isEqualTo(pageResponse.hasNext());
+			}
 
+			@Test
+			@DisplayName("페이징 요청 안 보낼 때 기본 값 넣어서 작동되는지 검증")
+			void checkDefaultPaging() {
 
+				// Given
+
+//				페이징 인자 캡처
+				ArgumentCaptor<PageRequest> pageableCaptor = ArgumentCaptor.forClass(PageRequest.class);
+
+//				mock 페이징 객체
+				PageImpl<GetBucketResDto> pageResponse = new PageImpl<>(Collections.emptyList());
+				given(bucketRepository.findAllBySecretIsFalseAndReportUnderLimit(any(PageRequest.class), eq(adminUtil.MAX_REPORT))).willReturn(pageResponse);
+
+				// When
+				communityService.getBucketWithComments(null, null);
+
+				// Then
+				/**
+				 * 기본값 잘 들어가는지 검증
+				 */
+				then(bucketRepository).should().findAllBySecretIsFalseAndReportUnderLimit(pageableCaptor.capture(), eq(adminUtil.MAX_REPORT));
+
+				PageRequest pageRequest = pageableCaptor.getValue();
+				assertThat(pageRequest.getPageNumber()).isEqualTo(0);
+				assertThat(pageRequest.getPageSize()).isEqualTo(10);
 
 			}
+
 		}
+
+
+
 
 		@Nested
 		@DisplayName("예외 케이스")
