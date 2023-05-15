d4f0ebc546919492e4d8904dd3e86e8a924c441c
dayong2eyo <dayong2eyo@gmail.com>
2023-05-15 15:04:35 +0900

    💩[FE]feat: 유언장 CSS변경

--- Frontend/frontend/src/pages/Will/Will.tsx
+++ Frontend/frontend/src/pages/Will/Will.tsx
@@ -2,9 +2,10 @@ import tw from 'twin.macro';
 import styled from 'styled-components';
 import { Link, useNavigate } from 'react-router-dom';
 import TopBar from '../../components/common/TopBar';
+import AppTitle from '../../assets/images/app_title.svg';
 
 const Container = styled.div`
-  ${tw`flex flex-col justify-center items-center p-[16px] m-[24px] bg-gray-100/80`}
+  ${tw`flex flex-col justify-center rounded-xl items-center p-[8px] m-[24px] bg-gray-100/80`}
 `;
 const LinkWrapper = styled.div`
   ${tw`flex w-full text-h3 justify-center items-center my-[8px]`}
@@ -15,17 +16,24 @@ function Will(): JSX.Element {
   return (
     <div>
       <TopBar title="유언장" />
+      <div className="flex justify-center my-[30px]">
+        <img src={AppTitle} alt="" />
+      </div>
       <Container>
         <Link to="/will/text">
           <LinkWrapper>
             <span>유언장 등록</span>
           </LinkWrapper>
         </Link>
+      </Container>
+      <Container>
         <Link to="/will/sign">
           <LinkWrapper>
             <span>유언장 서명 등록</span>
           </LinkWrapper>
         </Link>
+      </Container>
+      <Container>
         <Link to="/will/video">
           <LinkWrapper>
             <span>동영상 유언장</span>
