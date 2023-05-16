a153147236160c7d8a6d2ab82735c929dc0ea406
isks2 <isks2@naver.com>
2023-05-16 20:56:38 +0900

    👀[FE] style: 버킷, 다이어리 wrap 반응형으로 수정 및 현재 위치 써줌

--- Frontend/afterfront/src/pages/Bucket/BucketDetail.tsx
+++ Frontend/afterfront/src/pages/Bucket/BucketDetail.tsx
@@ -2,7 +2,6 @@ import { useParams } from 'react-router-dom';
 import { useRecoilValue } from 'recoil';
 import styled from 'styled-components';
 import tw from 'twin.macro';
-import backgroundImg from '../../assets/images/diary_bg.png';
 import TopBar from '../../components/common/TopBar';
 import { userDataState } from '../../states/UserDataState';
 import AuthWrapper from '../../api/AuthWrapper';
@@ -19,49 +18,23 @@ interface Bucket {
   updatedAt: string;
 }
 
-const Background = styled.div`
-  background-image: url(${backgroundImg});
-  background-size: cover;
-  background-position: center;
-  background-repeat: no-repeat;
-  min-height: 100vh;
-  width: 100%;
-  background-attachment: fixed;
-`;
-
 const Container = styled.div`
-  ${tw`pt-12 pb-24`}
+  ${tw`pt-12 pb-24 px-[24px]`}
 `;
 
 const BucketWrap = styled.div`
-  ${tw`mb-6 p-6 flex flex-col mx-auto`}
-  max-width: calc(100% - 48px);
+  ${tw`mb-6 p-6 flex flex-col mx-auto w-full sm:w-4/6 md:w-3/6`}
   background-color: rgba(246, 246, 246, 0.7);
   border-radius: 10px;
   position: relative;
 `;
 
 const BucketHeader = styled.div`
-  ${tw`flex`}// justify-content: space-between;
-`;
-
-const DotIcon = styled.div`
-  ${tw`flex`}
-  position: absolute;
-  right: 5%;
-  top: 18%;
-`;
-
-const CommentDotIcon = styled.div`
-  ${tw`flex`}
-  position: absolute;
-  right: 5%;
-  top: 40%;
+  ${tw`flex`};
 `;
 
 const ContentTitle = styled.div`
-  ${tw``}
-  width: 280px;
+  ${tw`w-full`}
   word-break: break-all;
   text-overflow: ellipsis;
   word-wrap: break-word;
@@ -86,73 +59,6 @@ const BucketText = styled.div`
   font-size: 15px;
 `;
 
-const CommentWrap = styled.div`
-  ${tw`mb-6 flex flex-col mx-auto`}
-  max-width: calc(100% - 48px);
-  border-radius: 10px;
-  // color: white;
-  // border: solid 1px white;
-`;
-
-const CommentBox = styled.div`
-  ${tw`mb-2 p-6 flex`}
-  justify-content: space-between;
-  color: black;
-  background-color: rgba(246, 246, 246, 0.7);
-  border-radius: 10px;
-  position: relative;
-`;
-
-const StyledCommentForm = styled.form`
-  ${tw`mb-6 flex mx-auto w-full`}
-  color: white;
-`;
-
-const StyledInput = styled.input`
-  ${tw`flex-1 mr-2`}
-  color: black;
-  border-radius: 10px;
-`;
-
-const StyledButton = styled.button`
-  ${tw`text-white px-4 py-2 rounded`}
-`;
-
-const CommentNick = styled.div`
-  ${tw``}
-  font-weight: bold;
-  font-size: 14px;
-`;
-
-const CommentDate = styled.div`
-  ${tw`mb-2`}
-  font-size: 12px;
-`;
-
-const CommentContent = styled.div`
-  ${tw``}
-  width: 280px;
-  font-size: 15px;
-`;
-
-const EditContentForm = styled.form`
-  ${tw`flex flex-col`}
-  align-items: flex-end;
-`;
-
-const EditContentInput = styled.textarea`
-  ${tw`p-1`}
-  width: 270px;
-  font-size: 15px;
-  height: auto;
-  border-radius: 5px;
-`;
-
-const EditButton = styled.button`
-  ${tw`ml-2`}
-  font-size: 14px;
-`;
-
 function BucketDetail() {
   const { bucketId } = useParams<{ bucketId: string }>();
   const userData = useRecoilValue(userDataState);
@@ -173,7 +79,7 @@ function BucketDetail() {
   }
   return (
     <AuthWrapper>
-      <TopBar title="" />
+      <TopBar title="버킷리스트" />
       <Container>
         <BucketWrap>
           <BucketHeader>
