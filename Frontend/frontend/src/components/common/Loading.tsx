ebc05f1a54e892106abda5016eb2e3147e9be6dd
SuGyoungIn <isks2838@gmail.com>
2023-05-17 00:34:09 +0900

    👀[FE] feat: 로딩페이지 구현

--- Frontend/frontend/src/components/common/Loading.tsx
+++ Frontend/frontend/src/components/common/Loading.tsx
@@ -1,19 +1,26 @@
 import React from 'react';
+import styled from 'styled-components';
+import tw from 'twin.macro';
 
 import Lottie from 'react-lottie-player';
 import LoadingFox from '../../assets/loading/loading_fox.json';
 
+const LoadingWrappper = styled.div`
+  ${tw`w-full bg-gray-100/70 flex flex-col justify-center z-10 fixed top-0`}
+  height: calc(100% - 70px);
+`;
+
 function Loading() {
   return (
-    <div>
+    <LoadingWrappper>
       <Lottie
         animationData={LoadingFox}
         play
         loop
         style={{ width: 120, height: 120, margin: '0 auto' }}
       />
-      <p> 잠시만 기다려주세요.</p>
-    </div>
+      <p className="text-p1 text-center"> 잠시만 기다려주세요.</p>
+    </LoadingWrappper>
   );
 }
 
