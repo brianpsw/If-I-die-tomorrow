4515b39d47e555f84677ddb95fb69eb283a804e2
Park Sang Woo <brianpsw@naver.com>
2023-05-17 16:31:06 +0900

    🍤[BE]feat: File Download File Download JIRA Code :

--- Backend/ifIDieTomorrow/src/main/java/com/a307/ifIDieTomorrow/domain/controller/AfterController.java
+++ Backend/ifIDieTomorrow/src/main/java/com/a307/ifIDieTomorrow/domain/controller/AfterController.java
@@ -2,14 +2,23 @@ package com.a307.ifIDieTomorrow.domain.controller;
 
 import com.a307.ifIDieTomorrow.domain.service.AfterService;
 import com.a307.ifIDieTomorrow.global.exception.NotFoundException;
+import com.a307.ifIDieTomorrow.global.util.FileUtil;
 import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.tags.Tag;
 import lombok.RequiredArgsConstructor;
+import org.springframework.core.io.Resource;
+import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
+import javax.servlet.http.HttpServletRequest;
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.IOException;
 import java.util.Map;
+import java.util.UUID;
 
 @Tag(name = "사후 페이지", description = "APIs for AFTER")
 @RestController
@@ -17,6 +26,7 @@ import java.util.Map;
 @RequestMapping("/after")
 public class AfterController {
     private final AfterService afterService;
+    private final FileUtil fileUtil;
 
     @PostMapping()
     @Operation(summary = "사후 페이지 전송", description = "사후 페이지 데이터를 전송받음.")
@@ -30,5 +40,31 @@ public class AfterController {
     public ResponseEntity<Map<String, Object>> getMyData(
             ) throws NotFoundException {
         return ResponseEntity.status(HttpStatus.OK).body(afterService.getMyData());
+    }
+
+    @GetMapping("/download")
+    public ResponseEntity<Resource> downloadFile(HttpServletRequest request) throws IOException, NotFoundException {
+        // Load file as Resource
+        String uuid = UUID.randomUUID().toString();
+        String fileName = afterService.downloadFile(uuid);
+        Resource resource = fileUtil.loadFileAsResource(fileName);
+
+        // Determine file's content type
+        String contentType = null;
+        try {
+            contentType = request.getServletContext().getMimeType(resource.getFile().getAbsolutePath());
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+        // Fallback to the default content type if type could not be determined
+        if (contentType == null) {
+            contentType = "application/octet-stream";
+        }
+
+        return ResponseEntity.ok()
+                .contentType(MediaType.parseMediaType(contentType))
+                .header(HttpHeaders.CONTENT_DISPOSITION, "attachment; filename=\"" + resource.getFilename() + "\"")
+                .body(resource);
     }
 }
--- Backend/ifIDieTomorrow/src/main/java/com/a307/ifIDieTomorrow/domain/service/AfterServiceImpl.java
+++ Backend/ifIDieTomorrow/src/main/java/com/a307/ifIDieTomorrow/domain/service/AfterServiceImpl.java
@@ -1,14 +1,24 @@
 package com.a307.ifIDieTomorrow.domain.service;
 
+import com.a307.ifIDieTomorrow.domain.dto.bucket.GetBucketResDto;
+import com.a307.ifIDieTomorrow.domain.dto.category.CreateCategoryResDto;
+import com.a307.ifIDieTomorrow.domain.dto.diary.GetDiaryResDto;
+import com.a307.ifIDieTomorrow.domain.dto.photo.GetPhotoByCategoryResDto;
+import com.a307.ifIDieTomorrow.domain.dto.photo.GetPhotoResDto;
+import com.a307.ifIDieTomorrow.domain.dto.will.GetWillByUserResDto;
 import com.a307.ifIDieTomorrow.domain.entity.After;
 import com.a307.ifIDieTomorrow.domain.repository.*;
 import com.a307.ifIDieTomorrow.global.auth.UserPrincipal;
 import com.a307.ifIDieTomorrow.global.exception.NotFoundException;
+import com.a307.ifIDieTomorrow.global.util.FileUtil;
 import lombok.RequiredArgsConstructor;
+import lombok.SneakyThrows;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Service;
 
+import java.io.IOException;
+import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.Map;
 
@@ -20,7 +30,9 @@ public class AfterServiceImpl implements AfterService{
     private final BucketRepository bucketRepository;
     private final DiaryRepository diaryRepository;
     private final PhotoService photoService;
+    private final WillService willService;
     private final WillRepository willRepository;
+    private final FileUtil fileUtil;
 
     @Override
     public Map<String, Object> getData(String pwd) throws NotFoundException {
@@ -39,7 +51,47 @@ public class AfterServiceImpl implements AfterService{
         result.put("photos", photoService.getPhotoByUser(((UserPrincipal) SecurityContextHolder.getContext().getAuthentication().getPrincipal()).getUserId()));
         result.put("diaries", diaryRepository.findAllByUserIdIdWithUserNickName(((UserPrincipal) SecurityContextHolder.getContext().getAuthentication().getPrincipal()).getUserId()));
         result.put("buckets", bucketRepository.findAllByUserIdWithUserNickName(((UserPrincipal) SecurityContextHolder.getContext().getAuthentication().getPrincipal()).getUserId()));
-        result.put("will", willRepository.getByUserId(((UserPrincipal) SecurityContextHolder.getContext().getAuthentication().getPrincipal()).getUserId()));
+        result.put("will", willService.getWillByUserId());
         return result;
+    }
+
+    @Override
+    public String downloadFile(String uuid) throws NotFoundException, IOException {
+        Map<String, Object> result = getMyData();
+
+        String basicDirectory = "./";
+        fileUtil.createDirectory(basicDirectory + uuid);
+        String commonDirectory = basicDirectory + uuid + "\\";
+        fileUtil.copyDirectory("./IIDT", basicDirectory + uuid);
+        fileUtil.downloadFile(((GetWillByUserResDto)result.get("will")).getVideoUrl(), commonDirectory + "will\\" + ((GetWillByUserResDto)result.get("will")).getVideoUrl().substring(((GetWillByUserResDto)result.get("will")).getVideoUrl().lastIndexOf("/") + 1) + fileUtil.getContentType(((GetWillByUserResDto)result.get("will")).getVideoUrl()));
+        fileUtil.downloadFile(((GetWillByUserResDto)result.get("will")).getSignUrl(), commonDirectory + "will\\" + ((GetWillByUserResDto)result.get("will")).getSignUrl().substring(((GetWillByUserResDto)result.get("will")).getSignUrl().lastIndexOf("/") + 1) + fileUtil.getContentType(((GetWillByUserResDto)result.get("will")).getSignUrl()));
+        for (GetBucketResDto item : (ArrayList<GetBucketResDto>)result.get("buckets")
+             ) {
+            if(item.getImageUrl() != null && !"".equals(item.getImageUrl())){
+                fileUtil.downloadFile(item.getImageUrl(), commonDirectory + "bucket\\" + item.getImageUrl().substring(item.getImageUrl().lastIndexOf("/")) + fileUtil.getContentType(item.getImageUrl()));
+            }
+        }
+        for (GetDiaryResDto item : (ArrayList<GetDiaryResDto>)result.get("diaries")
+        ) {
+            if(item.getImageUrl() != null && !"".equals(item.getImageUrl())){
+                fileUtil.downloadFile(item.getImageUrl(), commonDirectory + "diary\\"+ item.getImageUrl().substring(item.getImageUrl().lastIndexOf("/")) + fileUtil.getContentType(item.getImageUrl()));
+            }
+        }
+        for (GetPhotoByCategoryResDto item : (ArrayList<GetPhotoByCategoryResDto>)result.get("photos")
+             ) {
+
+            if(item.getCategory().getImageUrl() != null && !"".equals(item.getCategory().getImageUrl())){
+                fileUtil.downloadFile(item.getCategory().getImageUrl(), commonDirectory + "category\\" + (item.getCategory().getImageUrl().substring(item.getCategory().getImageUrl().lastIndexOf("/"))) + fileUtil.getContentType(item.getCategory().getImageUrl()));
+            }
+            for (GetPhotoResDto photo : item.getPhotos()
+                 ) {
+                if((photo.getImageUrl() != null && !"".equals(photo.getImageUrl()))){
+                    fileUtil.downloadFile(photo.getImageUrl(), commonDirectory + "photo\\" + photo.getImageUrl().substring(photo.getImageUrl().lastIndexOf("/")) + fileUtil.getContentType(photo.getImageUrl()));
+                }
+            }
+        }
+        fileUtil.zipDirectory(commonDirectory, basicDirectory + uuid + ".zip" );
+
+        return basicDirectory + uuid + ".zip";
     }
 }
