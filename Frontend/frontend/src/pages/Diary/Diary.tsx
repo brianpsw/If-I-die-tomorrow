0113eadd7347d06b79ff04a249a9cb97f9c4cb61
dayong2eyo <dayong2eyo@gmail.com>
2023-05-18 21:08:30 +0900

    💩[FE]fix: 다이어리 페이지 pwa용 높이 조절 및 로딩 화면 추가

--- Frontend/frontend/src/pages/Diary/Diary.tsx
+++ Frontend/frontend/src/pages/Diary/Diary.tsx
@@ -3,6 +3,7 @@ import { useRecoilState } from 'recoil';
 // import { calendarState } from '../../states/CalendarState';
 import { userState } from '../../states/UserState';
 import { useNavigate } from 'react-router-dom';
+import Swal from 'sweetalert2';
 import styled from 'styled-components';
 import tw from 'twin.macro';
 import requests from '../../api/config';
@@ -16,6 +17,7 @@ import uploadIcon from '../../assets/icons/camera_alt.svg';
 import CheckedIcon from '../../assets/icons/checked_box.svg';
 import UnCheckedIcon from '../../assets/icons/unchecked_box.svg';
 import surveyIcon from '../../assets/icons/survey.svg';
+import Loading from '../../components/common/Loading';
 
 import {
   CardWrap,
@@ -83,6 +85,7 @@ function Diary() {
   const [insight, setInsight] = useState<string | null>('');
   const [sameDay, setSameDay] = useState<boolean>(false);
   //Form controller
+  const [loadingOpen, setLoadingOpen] = useState<boolean>(false);
   const [formOpen, setFormOpen] = useState<boolean>(false);
   const [formOpen2, setFormOpen2] = useState<boolean>(false);
   const [isChecked, setIsChecked] = useState(false);
@@ -126,6 +129,7 @@ function Diary() {
     );
 
     const post_diary = async () => {
+      setLoadingOpen(true);
       try {
         await defaultApi.post(requests.POST_DIARY(), formData, {
           withCredentials: true,
@@ -133,9 +137,22 @@ function Diary() {
             'Content-Type': 'multipart/form-data',
           },
         });
+        setLoadingOpen(false);
 
+        Swal.fire({
+          title: '다이어리 완료 성공!',
+          icon: 'success',
+          timer: 1000,
+          showConfirmButton: false,
+        });
         get_user_diary();
       } catch (error) {
+        setLoadingOpen(false);
+        Swal.fire({
+          title: '다이어리 완료 실패...',
+          icon: 'error',
+          confirmButtonText: '확인',
+        });
         throw error;
       }
     };
@@ -221,8 +238,9 @@ function Diary() {
   //   }
   // }, [data, sameDay]);
   return (
-    <div className="w-full">
+    <div className="min-h-[100vh]">
       {/* <Background> */}
+      {loadingOpen ? <Loading /> : ''}
       <Container>
         <div className="flex w-full justify-between">
           <LogoContainer src={IIDT} />
