0ce5db0e9bc6463a6dd157954e14ffc5f99b3032
Park Sang Woo <brianpsw@naver.com>
2023-05-22 14:00:04 +0900

    Unique Constraint

--- Backend/ifIDieTomorrow/src/main/java/com/a307/ifIDieTomorrow/domain/entity/Token.java
+++ Backend/ifIDieTomorrow/src/main/java/com/a307/ifIDieTomorrow/domain/entity/Token.java
@@ -12,7 +12,7 @@ import javax.persistence.*;
 @NoArgsConstructor
 @AllArgsConstructor
 @Builder
-@Table(name = "token")
+@Table(name = "token", uniqueConstraints = @UniqueConstraint(columnNames = { "userId", "token" }))
 public class Token {
 
     @Id
