a3033f19b56f135419090fd74b529d758372c2d6
Park Sang Woo <brianpsw@naver.com>
2023-05-23 15:57:42 +0900

    Fix: delete typo

--- Frontend/frontend/src/pages/Home/Home.tsx
+++ Frontend/frontend/src/pages/Home/Home.tsx
@@ -1,4 +1,4 @@
-import { , useEffect } from 'react';
+import { useEffect } from 'react';
 import { useNavigate } from 'react-router';
 
 import { useSetRecoilState, useRecoilState, useRecoilValue } from 'recoil';
