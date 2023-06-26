592fa2269574656986bba55ba671ff0921407e51
이연수 <noprob1325@naver.com>
2023-06-26 11:51:39 +0900

    👻[BE]feat: PhotoServiceImplTest 코드 추가 아래의 테스트 코드가 추가되었습니다.
    카테고리 생성
      - 실패 케이스
        - 썸네일이 없는 카테고리 생성
        - 이름이 없는 카테고리 생성
    카테고리 조회
      - 성공 케이스
        - 내 카테고리 조회
    카테고리 수정(이름)
      - 성공 케이스
        - 이름이 입력된 카테고리 수정
      - 실패 케이스
        - 존재하지 않는 카테고리 수정
        - 다른 유저의 카테고리 수정
        - 이름이 입력되지 않은 카테고리 수정
    카테고리 수정(사진)
      - 성공 케이스
        - 사진이 입력된 카테고리 수정
      - 실패 케이스
        - 존재하지 않는 카테고리 수정
        - 다른 유저의 카테고리 수정
        - 사진이 입력되지 않은 카테고리 수정

--- Backend/ifIDieTomorrow/src/test/java/com/a307/ifIDieTomorrow/domain/service/PhotoServiceImplTest.java
+++ Backend/ifIDieTomorrow/src/test/java/com/a307/ifIDieTomorrow/domain/service/PhotoServiceImplTest.java
@@ -2,6 +2,8 @@ package com.a307.ifIDieTomorrow.domain.service;
 
 import com.a307.ifIDieTomorrow.domain.dto.category.CreateCategoryDto;
 import com.a307.ifIDieTomorrow.domain.dto.category.CreateCategoryResDto;
+import com.a307.ifIDieTomorrow.domain.dto.category.UpdateCategoryNameDto;
+import com.a307.ifIDieTomorrow.domain.dto.category.UpdateCategoryThumbnailDto;
 import com.a307.ifIDieTomorrow.domain.entity.Category;
 import com.a307.ifIDieTomorrow.domain.entity.User;
 import com.a307.ifIDieTomorrow.domain.repository.CategoryRepository;
@@ -10,6 +12,8 @@ import com.a307.ifIDieTomorrow.global.auth.ProviderType;
 import com.a307.ifIDieTomorrow.global.auth.UserPrincipal;
 import com.a307.ifIDieTomorrow.global.exception.IllegalArgumentException;
 import com.a307.ifIDieTomorrow.global.exception.NoPhotoException;
+import com.a307.ifIDieTomorrow.global.exception.NotFoundException;
+import com.a307.ifIDieTomorrow.global.exception.UnAuthorizedException;
 import com.a307.ifIDieTomorrow.global.util.ImageProcess;
 import com.a307.ifIDieTomorrow.global.util.S3Upload;
 import com.drew.imaging.ImageProcessingException;
@@ -26,12 +30,13 @@ import org.springframework.security.authentication.TestingAuthenticationToken;
 import org.springframework.security.core.context.SecurityContextHolder;
 
 import java.io.IOException;
+import java.util.List;
+import java.util.Optional;
 
 import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.BDDMockito.given;
 import static org.mockito.BDDMockito.then;
 import static org.mockito.Mockito.never;
-import static org.mockito.Mockito.times;
 
 @ExtendWith(MockitoExtension.class)
 public class PhotoServiceImplTest {
@@ -52,6 +57,7 @@ public class PhotoServiceImplTest {
 	private ImageProcess imageProcess;
 	
 	private User user;
+	private Category category0;
 	
 	@BeforeEach
 	void setUp() {
@@ -69,6 +75,13 @@ public class PhotoServiceImplTest {
 		UserPrincipal userPrincipal = UserPrincipal.create(user);
 		TestingAuthenticationToken authentication = new TestingAuthenticationToken(userPrincipal, null);
 		SecurityContextHolder.getContext().setAuthentication(authentication);
+		
+		category0 = Category.builder()
+				.categoryId(0L)
+				.name("test")
+				.imageUrl("test.com")
+				.userId(1L)
+				.build();
 	}
 	
 	@AfterEach
@@ -142,14 +155,37 @@ public class PhotoServiceImplTest {
 				
 				@Test
 				@DisplayName("썸네일이 없는 카테고리 생성")
-				void createCategoryWithoutThumbnail() {
-				
+				void createCategoryWithoutThumbnail() throws ImageProcessingException, IOException, IllegalArgumentException, MetadataException, NoPhotoException {
+					
+					// Given
+					CreateCategoryDto data = new CreateCategoryDto("test name");
+					
+					// When
+					
+					// Then
+					/* 동작 검증 */
+					then(s3Upload).shouldHaveNoInteractions();
+					BDDAssertions.thenThrownBy(() -> photoService.createCategory(data, null))
+							.isInstanceOf(NoPhotoException.class);
+					
 				}
 				
 				@Test
 				@DisplayName("이름이 없는 카테고리 생성")
 				void createCategoryWithoutName() {
-				
+					
+					// Given
+					CreateCategoryDto data = new CreateCategoryDto("");
+					MockMultipartFile photo = new MockMultipartFile("file", "test.jpg", "image/jpeg", "test".getBytes());
+					
+					// When
+					
+					// Then
+					/* 동작 검증 */
+					then(s3Upload).shouldHaveNoInteractions();
+					BDDAssertions.thenThrownBy(() -> photoService.createCategory(data, photo))
+							.isInstanceOf(IllegalArgumentException.class);
+					
 				}
 				
 			}
@@ -167,7 +203,22 @@ public class PhotoServiceImplTest {
 				@Test
 				@DisplayName("내 카테고리 조회")
 				void getCategory() {
-				
+					
+					// Given
+					List<CreateCategoryResDto> list = List.of(CreateCategoryResDto.toDto(category0));
+					
+					/* stubbing */
+					given(categoryRepository.findAllCategoryByUserId(1L)).willReturn(list);
+					
+					// When
+					List<CreateCategoryResDto> result = photoService.getCategory();
+					
+					// Then
+					BDDAssertions.then(result.size()).isEqualTo(1);
+					BDDAssertions.then(result).isEqualTo(list);
+					
+					then(categoryRepository).should().findAllCategoryByUserId(1L);
+					
 				}
 				
 			}
@@ -184,7 +235,33 @@ public class PhotoServiceImplTest {
 				
 				@Test
 				@DisplayName("이름이 입력된 카테고리 수정")
-				void updateCategory() {
+				void updateCategory() throws NotFoundException, IllegalArgumentException, UnAuthorizedException {
+					
+					// Given
+					UpdateCategoryNameDto data = new UpdateCategoryNameDto(0L, "test2");
+					
+					Category updatedCategory = Category.builder()
+							.categoryId(0L)
+							.userId(1L)
+							.name(data.getName())
+							.imageUrl("test.com")
+							.build();
+					
+					/* stubbing */
+					given(categoryRepository.save(any(Category.class))).willReturn(updatedCategory);
+					given(categoryRepository.findByCategoryId(0L)).willReturn(Optional.of(category0));
+					
+					// When
+					CreateCategoryResDto result = photoService.updateCategoryName(data);
+					
+					// Then
+					then(categoryRepository).should().findByCategoryId(data.getCategoryId());
+					then(s3Upload).shouldHaveNoInteractions();
+					
+					BDDAssertions.then(result.getCategoryId()).isEqualTo(0L);
+					BDDAssertions.then(result.getUserId()).isEqualTo(1L);
+					BDDAssertions.then(result.getName()).isEqualTo(data.getName());
+					BDDAssertions.then(result.getImageUrl()).isEqualTo(category0.getImageUrl());
 					
 				}
 				
@@ -197,19 +274,66 @@ public class PhotoServiceImplTest {
 				@Test
 				@DisplayName("존재하지 않는 카테고리 수정")
 				void updateCategoryWithWrongCategoryId() {
-				
+					
+					// Given
+					UpdateCategoryNameDto data = new UpdateCategoryNameDto(123L, "test2");
+					
+					// When
+					
+					// Then
+					BDDAssertions.thenThrownBy(() -> photoService.updateCategoryName(data))
+							.isInstanceOf(NotFoundException.class);
+					
+					then(categoryRepository).should(never()).save(any(Category.class));
+					then(s3Upload).shouldHaveNoInteractions();
+					
 				}
 				
 				@Test
 				@DisplayName("다른 유저의 카테고리 수정")
 				void updateCategoryOfOtherUser() {
-				
+					
+					// Given
+					UpdateCategoryNameDto data = new UpdateCategoryNameDto(1L, "test2");
+					
+					Category existingCategory = Category.builder()
+									.categoryId(1L)
+									.name("new")
+									.imageUrl("new.com")
+									.userId(2L)
+									.build();
+					
+					given(categoryRepository.findByCategoryId(data.getCategoryId())).willReturn(Optional.of(existingCategory));
+					
+					// When
+					
+					// Then
+					BDDAssertions.thenThrownBy(() -> photoService.updateCategoryName(data))
+							.isInstanceOf(UnAuthorizedException.class);
+					
+					then(categoryRepository).should(never()).save(any(Category.class));
+					then(s3Upload).shouldHaveNoInteractions();
+					
 				}
 				
 				@Test
 				@DisplayName("이름이 입력되지 않은 카테고리 수정")
 				void updateCategoryWithoutName() {
-				
+					
+					// Given
+					UpdateCategoryNameDto data = new UpdateCategoryNameDto(0L, "");
+					
+					given(categoryRepository.findByCategoryId(0L)).willReturn(Optional.of(category0));
+					
+					// When
+					
+					// Then
+					BDDAssertions.thenThrownBy(() -> photoService.updateCategoryName(data))
+							.isInstanceOf(IllegalArgumentException.class);
+					
+					then(categoryRepository).should(never()).save(any(Category.class));
+					then(s3Upload).shouldHaveNoInteractions();
+					
 				}
 				
 			}
@@ -226,7 +350,37 @@ public class PhotoServiceImplTest {
 				
 				@Test
 				@DisplayName("사진이 입력된 카테고리 수정")
-				void updateCategory() {
+				void updateCategory() throws ImageProcessingException, IOException, MetadataException, NotFoundException, IllegalArgumentException, NoPhotoException, UnAuthorizedException {
+					
+					// Given
+					UpdateCategoryThumbnailDto data = new UpdateCategoryThumbnailDto(0L);
+					MockMultipartFile photo = new MockMultipartFile("file", "test.jpg", "image/jpeg", "test".getBytes());
+					
+					Category updatedCategory = Category.builder()
+							.categoryId(0L)
+							.userId(1L)
+							.name("test")
+							.imageUrl("test2.com")
+							.build();
+					
+					/* stubbing */
+					given(categoryRepository.findByCategoryId(0L)).willReturn(Optional.of(category0));
+					given(imageProcess.resizeImage(photo, 100)).willReturn(photo);
+					given(s3Upload.upload(photo, "category")).willReturn("test2.com");
+					given(categoryRepository.save(any(Category.class))).willReturn(updatedCategory);
+					
+					// When
+					CreateCategoryResDto result = photoService.updateCategoryThumbnail(data, photo);
+					
+					// Then
+					then(categoryRepository).should().findByCategoryId(data.getCategoryId());
+					then(imageProcess).should().resizeImage(photo, 100);
+					then(s3Upload).should().delete(any(String.class));
+					then(s3Upload).should().upload(photo, "category");
+					
+					BDDAssertions.then(result.getCategoryId()).isEqualTo(0L);
+					BDDAssertions.then(result.getUserId()).isEqualTo(1L);
+					BDDAssertions.then(result.getImageUrl()).isEqualTo("test2.com");
 					
 				}
 				
@@ -239,18 +393,67 @@ public class PhotoServiceImplTest {
 				@Test
 				@DisplayName("존재하지 않는 카테고리 수정")
 				void updateCategoryWithWrongCategoryId() {
-				
+					
+					// Given
+					UpdateCategoryThumbnailDto data = new UpdateCategoryThumbnailDto(123L);
+					MockMultipartFile photo = new MockMultipartFile("file", "test.jpg", "image/jpeg", "test".getBytes());
+					
+					// When
+					
+					// Then
+					BDDAssertions.thenThrownBy(() -> photoService.updateCategoryThumbnail(data, photo))
+							.isInstanceOf(NotFoundException.class);
+					
+					then(categoryRepository).should(never()).save(any(Category.class));
+					then(s3Upload).shouldHaveNoInteractions();
+					
 				}
 				
 				@Test
 				@DisplayName("다른 유저의 카테고리 수정")
 				void updateCategoryOfOtherUser() {
-				
+					
+					// Given
+					UpdateCategoryThumbnailDto data = new UpdateCategoryThumbnailDto(1L);
+					MockMultipartFile photo = new MockMultipartFile("file", "test.jpg", "image/jpeg", "test".getBytes());
+					
+					Category existingCategory = Category.builder()
+							.categoryId(1L)
+							.name("new")
+							.imageUrl("new.com")
+							.userId(2L)
+							.build();
+					
+					given(categoryRepository.findByCategoryId(1L)).willReturn(Optional.of(existingCategory));
+					
+					// When
+					
+					// Then
+					BDDAssertions.thenThrownBy(() -> photoService.updateCategoryThumbnail(data, photo))
+							.isInstanceOf(UnAuthorizedException.class);
+					
+					then(categoryRepository).should(never()).save(any(Category.class));
+					then(s3Upload).shouldHaveNoInteractions();
+					
 				}
 				
 				@Test
 				@DisplayName("사진이 입력되지 않은 카테고리 수정")
 				void updateCategoryWithoutThumbnail() {
+					
+					// Given
+					UpdateCategoryThumbnailDto data = new UpdateCategoryThumbnailDto(0L);
+					
+					given(categoryRepository.findByCategoryId(0L)).willReturn(Optional.of(category0));
+					
+					// When
+					
+					// Then
+					BDDAssertions.thenThrownBy(() -> photoService.updateCategoryThumbnail(data, null))
+							.isInstanceOf(NoPhotoException.class);
+					
+					then(categoryRepository).should(never()).save(any(Category.class));
+					then(s3Upload).shouldHaveNoInteractions();
 					
 				}
 				
